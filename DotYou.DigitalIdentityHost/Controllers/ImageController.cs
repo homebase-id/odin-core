848634a06a42b24b935872f528b1f02c74b923d8
Todd Mitchell <todd@vidence.co>
2021-05-18 18:23:17 -0700

    chat WIP

---
+++ DotYou.DigitalIdentityHost/Controllers/ImageController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DotYou.Kernel.Services.Authorization;
+using DotYou.Kernel.Services.Contacts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotYou.DigitalIdentityHost.Controllers
+{
+    [ApiController]
+    [Route("api/images")]
+    //[Authorize(Policy = DotYouPolicyNames.IsDigitalIdentityOwner)]
+    public class ImageController : ControllerBase
+    {
+        IContactService _contactService;
+
+        public ImageController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        
+        [HttpGet("avatar/{identifier}")]
+        public IActionResult Get(string identifier)
+        {
+            //TODO: read from cache or contact list by identifier
+            //return new JsonResult(new AvatarUri() {Uri = "/assets/unknown.png"});
+            
+            string root = $"wwwroot";
+            string file = $"{root}\\samples\\{identifier}.jpg";
+            string type = "image/jpeg";
+            if (!System.IO.File.Exists(file))
+            {
+                file = $"{root}\\assets\\unknown.png";
+                type = "image/png";
+            }
+
+            Byte[] b = System.IO.File.ReadAllBytes(file);
+            var result = File(b, type);
+            return result;
+        }
+
+
+    }
+}
