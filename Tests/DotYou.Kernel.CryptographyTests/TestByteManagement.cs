5de2ed6694e41d24e85837d7d60d03efe1b82bd9
Michael Seifert <michael@corleone.com>
2021-07-23 11:46:56 -0700

    Moved ByteArray class test into its own test

---
+++ Tests/DotYou.Kernel.CryptographyTests/TestByteManagement.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+using DotYou.Kernel.Cryptography;
+using DotYou.Kernel.Services.Admin.Authentication;
+using DotYou.Types.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using NUnit.Framework;
+
+namespace DotYou.Kernel.CryptographyTests
+{
+    public class TestByteManagement
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+        //
+        // ===== A FEW BYTE ARRAY TESTS =====
+        //
+
+        [Test]
+        public void CompareTwoRndFail()
+        {
+            byte[] ba1 = YFByteArray.GetRndByteArray(40);
+            byte[] ba2 = YFByteArray.GetRndByteArray(40);
+
+            if (YFByteArray.EquiByteArrayCompare(ba1, ba2))
+                Assert.Fail();
+            else
+                Assert.Pass();
+        }
+
+        [Test]
+        public void CompareArrayPass()
+        {
+            byte[] ba1 = YFByteArray.GetRndByteArray(40);
+
+            if (YFByteArray.EquiByteArrayCompare(ba1, ba1))
+                Assert.Pass();
+            else
+                Assert.Fail();
+        }
+
+
+
+    }
+}
--- Tests/DotYou.Kernel.CryptographyTests/UnitTestRndXor.cs
+++ Tests/DotYou.Kernel.CryptographyTests/UnitTestRndXor.cs
@@ -17,33 +17,6 @@ namespace DotYou.Kernel.CryptographyTests
         }
 
         //
-        // ===== A FEW BYTE ARRAY TESTS =====
-        //
-
-        [Test]
-        public void CompareTwoRndFail()
-        {
-            byte[] ba1 = YFByteArray.GetRndByteArray(40);
-            byte[] ba2 = YFByteArray.GetRndByteArray(40);
-
-            if (YFByteArray.EquiByteArrayCompare(ba1, ba2))
-                Assert.Fail();
-            else
-                Assert.Pass();
-        }
-
-        [Test]
-        public void CompareArrayPass()
-        {
-            byte[] ba1 = YFByteArray.GetRndByteArray(40);
- 
-            if (YFByteArray.EquiByteArrayCompare(ba1, ba1))
-                Assert.Pass();
-            else
-                Assert.Fail();
-        }
-
-        //
         // ===== AES CBC TESTS =====
         //
 
