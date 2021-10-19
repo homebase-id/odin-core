f7c57519cf9acb79a3a33770dc97d87a9f7acd48
Michael Seifert <michael@corleone.com>
2021-10-19 14:14:44 -0700

    working on appdeviceregistration

--- Youverse.Core.Services/Authorization/AppRegistration/AppRegistration.cs
+++ Youverse.Core.Services/Authorization/AppRegistration/AppRegistration.cs
@@ -32,7 +32,7 @@ namespace Youverse.Core.Services.Authorization.AppRegistration
     {
         public Guid Id { get; set; }
         
-        public byte[] DeviceAppToken { get; set; }
+        public byte[] DeviceAppToken { get; set; }  // This is half the AppDek
 
         public byte[] SharedSecret { get; set; } // The secret shared with the client. We need one per client
 
