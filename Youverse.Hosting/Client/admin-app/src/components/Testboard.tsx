d0598feee5dbcbb196c589e478539e329c47590e
Todd Mitchell <todd@vidence.co>
2021-12-02 22:45:21 -0800

    adminapp wip

--- Youverse.Hosting/Client/admin-app/src/components/Testboard.tsx
+++ Youverse.Hosting/Client/admin-app/src/components/Testboard.tsx
@@ -4,6 +4,36 @@ function Testboard(props: any) {
     
     return (
         <>
+            <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                <div className="feature col">
+                    <div className="feature-icon bg-primary bg-gradient">
+                    </div>
+                    <h2>Featured title</h2>
+                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                    <a href="#" className="icon-link">
+                        Call to action
+                    </a>
+                </div>
+                <div className="feature col">
+                    <div className="feature-icon bg-primary bg-gradient">
+                    </div>
+                    <h2>Featured title</h2>
+                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                    <a href="#" className="icon-link">
+                        Call to action
+                    </a>
+                </div>
+                <div className="feature col">
+                    <div className="feature-icon bg-primary bg-gradient">
+                    </div>
+                    <h2>Featured title</h2>
+                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                    <a href="#" className="icon-link">
+                        Call to action
+                    </a>
+                </div>
+            </div>
+            
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 className="h2">Dashboard</h1>
                 <div className="btn-toolbar mb-2 mb-md-0">
