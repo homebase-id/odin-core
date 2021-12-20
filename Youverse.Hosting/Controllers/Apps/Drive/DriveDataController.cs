5d9c8b9906a0daa5568f6ac9cedc4a1d94a39577
Todd Mitchell <todd@vidence.co>
2021-12-20 11:45:09 -0800

    storage and drives WIP

---
+++ Youverse.Hosting/Controllers/Apps/Drive/DriveDataController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Youverse.Hosting.Authentication.App;
+
+namespace Youverse.Hosting.Controllers.Apps.Drive
+{
+    [ApiController]
+    [Route("/api/app/v1/drive")]
+    [Authorize(Policy = AppPolicies.IsAuthorizedApp, AuthenticationSchemes = AppAuthConstants.AppAuthSchemeName)]
+    public class DriveDataController : Controller
+    {
+        
+
+    }
+}
