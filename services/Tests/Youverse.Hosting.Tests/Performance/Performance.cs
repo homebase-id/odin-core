28390aad51e5f566b53f730ceec2b934210da92a
Seifert69 <michael@corleone.com>
2022-12-15 09:57:47 +0100

    just minor edits

--- services/Tests/Youverse.Hosting.Tests/Performance/Performance.cs
+++ services/Tests/Youverse.Hosting.Tests/Performance/Performance.cs
@@ -28,8 +28,8 @@ namespace Youverse.Hosting.Tests.Performance
     public class MyPerformance
     {
         // For the performance test
-        private const int MAXTHREADS = 64;   // Should be at least 2 * your CPU cores. Can still be nice to test sometimes with lower. And not too high.
-        const int MAXITERATIONS = 5000;  // A number high enough to get warmed up and reliable
+        private const int MAXTHREADS = 1;   // Should be at least 2 * your CPU cores. Can still be nice to test sometimes with lower. And not too high.
+        const int MAXITERATIONS = 10000;  // A number high enough to get warmed up and reliable
 
 
         [Test]
@@ -56,6 +56,7 @@ namespace Youverse.Hosting.Tests.Performance
                     }
                 });
             }
+
 
             Task.WaitAll(tasks);
             sw.Stop();
@@ -233,6 +234,9 @@ namespace Youverse.Hosting.Tests.Performance
                 });
 
                 var publishResponse = await staticFileSvc.Publish(publishRequest);
+                if (!publishResponse.IsSuccessStatusCode)
+                    Console.WriteLine("staticFileSvc.Publish(publishRequest): " + publishResponse.ReasonPhrase);
+
                 Assert.True(publishResponse.IsSuccessStatusCode, publishResponse.ReasonPhrase);
                 Assert.NotNull(publishResponse.Content);
 
@@ -256,6 +260,8 @@ namespace Youverse.Hosting.Tests.Performance
 
                     // Do all the work here
                     var getFileResponse = await getStaticFileSvc.GetStaticFile(publishRequest.Filename);
+                    if (!getFileResponse.IsSuccessStatusCode)
+                        Console.WriteLine("GetStaticFile(): " + getFileResponse.ReasonPhrase);
                     Assert.True(getFileResponse.IsSuccessStatusCode, getFileResponse.ReasonPhrase);
                     Assert.IsNotNull(getFileResponse.Content);
 
