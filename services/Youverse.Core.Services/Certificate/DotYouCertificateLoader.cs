0d232d2a0337405f24bb8eeca07f497e07d8d0d6
Todd Mitchell <t@lighttheory.co>
2023-01-13 11:27:42 -0800

    updated cert loader to use lazy cache

--- services/Youverse.Core.Services/Certificate/DotYouCertificateLoader.cs
+++ services/Youverse.Core.Services/Certificate/DotYouCertificateLoader.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.IO;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
+using LazyCache;
 using Serilog;
 using Youverse.Core.Exceptions;
 
@@ -10,62 +11,52 @@ namespace Youverse.Core.Services.Certificate;
 
 public static class DotYouCertificateLoader
 {
-    private static readonly ConcurrentDictionary<string, X509Certificate2> _tempCache = new ConcurrentDictionary<string, X509Certificate2>(StringComparer.InvariantCultureIgnoreCase);
-
+    private static readonly IAppCache _cache = new CachingService();
+    
     public static X509Certificate2 LoadCertificate(string publicKeyPath, string privateKeyPath, bool failIfInvalid = false)
     {
-        if (File.Exists(publicKeyPath) == false || File.Exists(privateKeyPath) == false)
+        var policy = new LazyCacheEntryOptions()
         {
-            if(failIfInvalid)
-            {
-                throw new YouverseSystemException("Cannot find certificate or key file(s)");
-            }
-
-            return null;
-        }
+        };
         
         string key = publicKeyPath.ToLower();
-        if (_tempCache.TryGetValue(key, out var cachedCert))
+        return _cache.GetOrAdd(key, entry =>
         {
-            Log.Debug($"Cert Loader - from cache: {publicKeyPath}");
-            return cachedCert;
-        }
+            Log.Debug($"Cert Loader - from disk: {publicKeyPath}");
 
-        Log.Debug($"Cert Loader - from disk: {publicKeyPath}");
-
-
-        using (X509Certificate2 publicKey = new X509Certificate2(publicKeyPath))
-        {
-            string encodedKey = File.ReadAllText(privateKeyPath);
-            RSA rsaPrivateKey;
-            using (rsaPrivateKey = RSA.Create())
+            using (X509Certificate2 publicKey = new X509Certificate2(publicKeyPath))
             {
-                rsaPrivateKey.ImportFromPem(encodedKey.ToCharArray());
-        
-                if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                string encodedKey = File.ReadAllText(privateKeyPath);
+                RSA rsaPrivateKey;
+                using (rsaPrivateKey = RSA.Create())
                 {
-                    using (X509Certificate2 pubPrivEphemeral = publicKey.CopyWithPrivateKey(rsaPrivateKey))
+                    rsaPrivateKey.ImportFromPem(encodedKey.ToCharArray());
+
+                    if (Environment.OSVersion.Platform == PlatformID.Win32NT)
                     {
-                        // Export as PFX and re-import if you want "normal PFX private key lifetime"
-                        // (this step is currently required for SslStream, but not for most other things
-                        // using certificates)
-                        return new X509Certificate2(pubPrivEphemeral.Export(X509ContentType.Pfx));
+                        using (X509Certificate2 pubPrivEphemeral = publicKey.CopyWithPrivateKey(rsaPrivateKey))
+                        {
+                            // Export as PFX and re-import if you want "normal PFX private key lifetime"
+                            // (this step is currently required for SslStream, but not for most other things
+                            // using certificates)
+                            return new X509Certificate2(pubPrivEphemeral.Export(X509ContentType.Pfx));
+                        }
                     }
+                    else
+                    {
+                        return publicKey.CopyWithPrivateKey(rsaPrivateKey);
+                    }
+
+                    //// Disabled this part as it causes too many changes within Keychain causing Chrome to not open the Page:
+                    // using (X509Certificate2 pubPrivEphemeral = publicKey.CopyWithPrivateKey(rsaPrivateKey))
+                    // {
+                    //     // Export as PFX and re-import if you want "normal PFX private key lifetime"
+                    //     // (this step is currently required for SslStream, but not for most other things
+                    //     // using certificates)
+                    //     return new X509Certificate2(pubPrivEphemeral.Export(X509ContentType.Pfx));
+                    // }
                 }
-                else
-                {
-                    return publicKey.CopyWithPrivateKey(rsaPrivateKey);
-                }
-        
-                //// Disabled this part as it causes too many changes within Keychain causing Chrome to not open the Page:
-                // using (X509Certificate2 pubPrivEphemeral = publicKey.CopyWithPrivateKey(rsaPrivateKey))
-                // {
-                //     // Export as PFX and re-import if you want "normal PFX private key lifetime"
-                //     // (this step is currently required for SslStream, but not for most other things
-                //     // using certificates)
-                //     return new X509Certificate2(pubPrivEphemeral.Export(X509ContentType.Pfx));
-                // }
             }
-        }
+        }, policy);
     }
 }
