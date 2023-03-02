19fd1a4b028ea1ed4a943a59b601918afd6c78aa
Todd Mitchell <t@lighttheory.co>
2023-03-02 11:39:41 -0800

    removed rule for innocuous condition

--- services/Youverse.Core.Services/Drives/FileSystem/Base/Upload/UploadInstructionSet.cs
+++ services/Youverse.Core.Services/Drives/FileSystem/Base/Upload/UploadInstructionSet.cs
@@ -17,7 +17,7 @@ namespace Youverse.Core.Services.Drives.FileSystem.Base.Upload
             TransitOptions = new TransitOptions();
             StorageOptions = new StorageOptions();
         }
-        
+
         /// <summary>
         /// The transfer initialization vector used to encrypt the KeyHeader
         /// </summary>
@@ -39,13 +39,16 @@ namespace Youverse.Core.Services.Drives.FileSystem.Base.Upload
                 throw new YouverseClientException("Target drive is invalid", YouverseClientErrorCode.InvalidTargetDrive);
             }
 
-            if (TransitOptions != null)
-            {
-                if (TransitOptions.IsTransient && TransitOptions.UseGlobalTransitId)
-                {
-                    throw new YouverseClientException("Cannot use GlobalTransitId on a transient file.", YouverseClientErrorCode.CannotUseGlobalTransitIdOnTransientFile);
-                }
-            }
+            //Removed because this conflicts with AllowDistribution flag.
+            //Having UseGlobalTransitId with a transient file does not hurt anything;  
+            //it's just illogical because the file is going to be deleted
+            // if (TransitOptions != null)
+            // {
+            //     if (TransitOptions.IsTransient && TransitOptions.UseGlobalTransitId)
+            //     {
+            //         throw new YouverseClientException("Cannot use GlobalTransitId on a transient file.", YouverseClientErrorCode.CannotUseGlobalTransitIdOnTransientFile);
+            //     }
+            // }
         }
 
 
