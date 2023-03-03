3d1114162d4b18a7b55f34cecfa3df047cfb8808
Seifert69 <michael@corleone.com>
2023-03-03 15:15:00 +0100

    crudify

--- lib/Tests/Youverse.Core.Storage.Tests/IdentityDatabaseTests/TableFollowsMeTests.cs
+++ lib/Tests/Youverse.Core.Storage.Tests/IdentityDatabaseTests/TableFollowsMeTests.cs
@@ -178,27 +178,11 @@ namespace IdentityDatabaseTests
             using var db = new IdentityDatabase("URI=file:.\\follower-delete-01.db");
             db.CreateDatabase();
 
-            bool ok = false;
-            try
-            {
-                db.tblFollowsMe.DeleteFollower(null);
-            }
-            catch
-            {
-                ok = true;
-            }
-            Debug.Assert(ok);
+            int i = db.tblFollowsMe.DeleteFollower(null);
+            Debug.Assert(i == 0);
 
-            var hi = new byte[3];
-            try
-            {
-                db.tblFollowsMe.DeleteFollower("");
-            }
-            catch
-            {
-                ok = true;
-            }
-            Debug.Assert(ok);
+            i = db.tblFollowsMe.DeleteFollower("");
+            Debug.Assert(i == 0);
         }
 
 
