c18684907c06481f63278a9c7f2420f2687c7650
toddmitchell <t@lighttheory.co>
2023-03-06 16:23:59 -0800

    Shifted to tblCron

--- services/Youverse.Core.Services/Transit/Outbox/PendingTransfersService.cs
+++ services/Youverse.Core.Services/Transit/Outbox/PendingTransfersService.cs
@@ -6,31 +6,34 @@ using System.Threading.Tasks;
 using Dawn;
 using Microsoft.Extensions.Logging;
 using Youverse.Core.Identity;
+using Youverse.Core.Serialization;
 using Youverse.Core.Storage.Sqlite.IdentityDatabase;
+using Youverse.Core.Storage.Sqlite.ServerDatabase;
 using Youverse.Core.Util;
 
 namespace Youverse.Core.Services.Transit.Outbox
 {
     public class PendingTransfersService : IPendingTransfersService
     {
-        private IdentityDatabase _db;  // TODO: This looks incorrect, it should fetch the DB object from somewhere shouldn't it?
+        private readonly ServerDatabase _db;
 
         public PendingTransfersService(string dataPath)
         {
             Guard.Argument(dataPath, nameof(dataPath)).NotNull().NotEmpty();
             var finalPath = PathUtil.OsIfy(dataPath);
-            
+
             if (!Directory.Exists(finalPath))
             {
                 Directory.CreateDirectory(finalPath!);
-                Utils.ShellExecute($"chmod -R +rw {finalPath}");
-
+                // Utils.ShellExecute($"chmod -R +rw {finalPath}");
             }
-            var filePath = PathUtil.OsIfy($"{dataPath}{Path.PathSeparator}xfer.db");
-            
-            Utils.ShellExecute($"chmod -R +rw {filePath}");
 
-            _db = new IdentityDatabase($"Data Source={filePath}");
+            var filePath = PathUtil.OsIfy($"{dataPath}{Path.PathSeparator}xfer.db");
+
+            // Utils.ShellExecute($"chmod -R +rw {filePath}");
+
+            // _db = new IdentityDatabase($"Data Source={filePath}");
+            _db = new ServerDatabase($"Data Source={filePath}");
             _db.CreateDatabase(false);
         }
 
@@ -41,14 +44,14 @@ namespace Youverse.Core.Services.Transit.Outbox
 
         public void EnsureIdentityIsPending(OdinId sender)
         {
-            //Note: I use sender here because boxId has a unique constraint; and we only a sender in this table once.
-            //I swallow the exception because there's no direct way to see if a record exists for this sender already
-            // byte[] fileId = new byte[] { 1, 1, 2, 3, 5 };
-            Guid fileId = Guid.NewGuid();
             try
             {
-                // _db.tblOutbox.InsertRow(sender.ToGuidIdentifier().ToByteArray(), fileId, 0, sender.Id.ToLower().ToUtf8ByteArray());
-                _db.tblOutbox.Insert(new OutboxItem() { boxId = sender.ToHashId(), fileId = fileId, recipient = sender.Id.ToLower(), priority = 0, value = sender.Id.ToLower().ToUtf8ByteArray() });
+                _db.tblCron.Insert(new CronItem()
+                {
+                    identityId = sender,
+                    type = 1,
+                    data = sender.Id.ToLower().ToUtf8ByteArray(),
+                });
             }
             catch (Microsoft.Data.Sqlite.SqliteException ex)
             {
@@ -63,21 +66,21 @@ namespace Youverse.Core.Services.Transit.Outbox
 
         public async Task<(IEnumerable<OdinId>, Guid marker)> GetIdentities()
         {
-            var records = _db.tblOutbox.PopAll(out var marker);
+            var records = _db.tblCron.Pop(1, out var marker);
 
-            var senders = records.Select(item => new OdinId(item.value.ToStringFromUtf8Bytes())).ToList();
+            var senders = records.Select(item => new OdinId(item.data.ToStringFromUtf8Bytes())).ToList();
 
             return (senders, marker);
         }
 
         public void MarkComplete(Guid marker)
         {
-            _db.tblOutbox.PopCommit(marker);
+            _db.tblCron.PopCommitList(new List<Guid>() { marker });
         }
 
         public void MarkFailure(Guid marker)
         {
-            _db.tblOutbox.PopCancel(marker);
+            _db.tblCron.PopCancelList(new List<Guid>() { marker });
         }
     }
 }
