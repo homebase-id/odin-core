3a3409cf590269057acb9af5fcd890d741e3ffb3
toddmitchell <t@lighttheory.co>
2023-03-10 23:21:08 -0800

    added drive filtering

--- services/Youverse.Core.Services/AppNotifications/DeviceSocket.cs
+++ services/Youverse.Core.Services/AppNotifications/DeviceSocket.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Net.WebSockets;
 using Youverse.Core.Services.Authorization.ExchangeGrants;
+using Youverse.Core.Services.Drives;
 
 namespace Youverse.Core.Services.AppNotifications;
 
@@ -9,4 +11,10 @@ public class DeviceSocket
     public Guid Key { get; set; }
     public WebSocket Socket { get; set; }
     public ClientAuthenticationToken DeviceAuthToken { get; set; }
+    
+    /// <summary>
+    /// List of drives to which this device socket is subscribed
+    /// </summary>
+    public List<Guid> Drives { get; set; }
+        
 }
