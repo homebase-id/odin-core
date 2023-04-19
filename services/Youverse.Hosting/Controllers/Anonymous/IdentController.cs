2a9e29c012e476403dcc1c2139b63698bcc08624
toddmitchell <t@lighttheory.co>
2023-04-19 09:36:49 -0700

    added controller

---
+++ services/Youverse.Hosting/Controllers/Anonymous/IdentController.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Youverse.Hosting.Controllers.Anonymous
+{
+    [ApiController]
+    [Route(YouAuthApiPathConstants.AuthV1)]
+    public class IdentController : Controller
+    {
+        /// <summary>
+        /// Identifies this server as an ODIN identity server
+        /// </summary>
+        [HttpGet("ident")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetInfo()
+        {
+            return new JsonResult(new
+            {
+                id = 42
+            });
+        }
+    }
+}
