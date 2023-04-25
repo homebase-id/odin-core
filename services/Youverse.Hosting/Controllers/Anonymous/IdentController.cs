d3163e0f137d7ce8daa31ab0e7e8b06652fa07ed
toddmitchell <t@lighttheory.co>
2023-04-25 11:04:21 -0700

    added validation for ident

--- services/Youverse.Hosting/Controllers/Anonymous/IdentController.cs
+++ services/Youverse.Hosting/Controllers/Anonymous/IdentController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Youverse.Core.Services.Base;
+using Youverse.Core.Services.Registry;
 
 namespace Youverse.Hosting.Controllers.Anonymous
 {
@@ -10,10 +11,13 @@ namespace Youverse.Hosting.Controllers.Anonymous
     public class IdentController : Controller
     {
         private readonly DotYouContextAccessor _dotYouContextAccessor;
+        private readonly IIdentityRegistry _registry;
 
-        public IdentController(DotYouContextAccessor dotYouContextAccessor)
+
+        public IdentController(DotYouContextAccessor dotYouContextAccessor, IIdentityRegistry registry)
         {
             _dotYouContextAccessor = dotYouContextAccessor;
+            _registry = registry;
         }
 
         /// <summary>
@@ -23,9 +27,18 @@ namespace Youverse.Hosting.Controllers.Anonymous
         [Produces("application/json")]
         public async Task<IActionResult> GetInfo()
         {
+            var tenant = _dotYouContextAccessor.GetCurrent().Tenant;
+            if(await _registry.IsIdentityRegistered(tenant))
+            {
+                return await Task.FromResult(new JsonResult(new
+                {
+                    OdinId = tenant
+                }));
+            }
+            
             return await Task.FromResult(new JsonResult(new
             {
-                OdinId = _dotYouContextAccessor.GetCurrent().Tenant
+                OdinId = string.Empty
             }));
         }
     }
