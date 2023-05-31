110055057d8b68926b752fd366ce74accc0fca03
Sebastian Bargmann <sebbarg@gmail.com>
2023-05-31 13:59:55 +0200

    Adding http exception types

---
+++ lib/Youverse.Core/Exceptions/Client/ForbiddenException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+
+namespace Youverse.Core.Exceptions.Client;
+
+public class ForbiddenException : ClientException
+{
+    public const string DefaultErrorMessage = "Forbidden";
+
+    public ForbiddenException(
+        string message = DefaultErrorMessage,
+        HttpStatusCode httpStatusCode = HttpStatusCode.Forbidden,
+        Exception inner = null
+        ) : base(
+            message,
+            httpStatusCode,
+            inner
+        )
+    {
+    }
+}
+
