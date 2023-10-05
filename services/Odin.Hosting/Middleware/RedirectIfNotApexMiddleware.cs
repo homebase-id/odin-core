e6c164529a6083f8c0e6e0726bcf8187a139be60
Sebastian Bargmann <sebbarg@gmail.com>
2023-10-05 16:23:58 +0200

    Revert "For now we only fall through to the redirect if prefix label is 'www'"
    This reverts commit e94c68ef9b5be7c25e6a3533d4de5cd999cfa3e7.

--- services/Odin.Hosting/Middleware/RedirectIfNotApexMiddleware.cs
+++ services/Odin.Hosting/Middleware/RedirectIfNotApexMiddleware.cs
@@ -28,7 +28,6 @@ public class RedirectIfNotApexMiddleware
 
     public async Task Invoke(HttpContext context)
     {
-        // non-http needed by certificate creation, so no redirecting!
         if (!context.Request.IsHttps)
         {
             await _next(context);
@@ -54,15 +53,7 @@ public class RedirectIfNotApexMiddleware
             return;
         }
 
-        var hostName = context.Request.Host.Host;
-        if (tenant.Name == hostName)
-        {
-            await _next(context);
-            return;
-        }
-
-        // For now we only fall through to the redirect if prefix label is 'www'
-        if (hostName != $"www.{tenant.Name}")
+        if (tenant.Name == context.Request.Host.Host)
         {
             await _next(context);
             return;
