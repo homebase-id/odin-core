6302b1cc9fc52b51c807aac7285769e3f5ffe08d
todd mitchell <1205927+toddmitchell@users.noreply.github.com>
2023-10-17 14:58:43 -0700

    added resiliency for query batch; added drive query api for get by client uniqueId (#344)
    Co-authored-by: toddmitchell <t@lighttheory.co>

--- services/Odin.Hosting/Controllers/ClientToken/App/AppApiPathConstants.cs
+++ services/Odin.Hosting/Controllers/ClientToken/App/AppApiPathConstants.cs
@@ -15,7 +15,6 @@
         public const string TransitQueryV1 = TransitV1 + "/query";
         
         public const string TransitReactionContentV1 = TransitV1 + "/reactions";
-
         
         public const string CirclesV1 = BasePathV1 + "/circles";
         
@@ -24,6 +23,12 @@
         public const string FollowersV1 = CirclesV1 + "/followers";
 
         public const string DriveV1 = BasePathV1 + "/drive";
+
+        public const string DriveQueryV1 = DriveV1 + "/query";
+        
+        public const string DriveQuerySpecializedV1 = DriveQueryV1 + "/specialized";
+        
+        public const string DriveQuerySpecializedClientUniqueId = DriveQuerySpecializedV1 + "/cuid";
 
         public const string SecurityV1 = BasePathV1 + "/security";
 
