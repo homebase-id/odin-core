7860a4620f3673a5988fc008eff36b421098488c
toddmitchell <t@lighttheory.co>
2023-10-24 19:50:11 -0700

    fixed deleted comment issues

--- services/Tests/Odin.Hosting.Tests/OwnerApi/Drive/CommentFileSystem/CommentFsOwnerTests.cs
+++ services/Tests/Odin.Hosting.Tests/OwnerApi/Drive/CommentFileSystem/CommentFsOwnerTests.cs
@@ -64,6 +64,58 @@ namespace Odin.Hosting.Tests.OwnerApi.Drive.CommentFileSystem
             Assert.IsTrue(commentFileHeader.FileMetadata.AppData.FileType == commentMetadata.AppData.FileType);
         }
 
+        [Test]
+        public async Task ReactionPreviewUpdatedWhenCommentDeleted()
+        {
+            var client = _scaffold.CreateOwnerApiClient(TestIdentities.Samwise);
+            var drive = await client.Drive.CreateDrive(TargetDrive.NewTargetDrive(), "Test Drive", "", false, true);
+
+            var blogMetadata = new UploadFileMetadata()
+            {
+                ContentType = "text/plain",
+                AppData = new UploadAppFileMetaData()
+                {
+                    FileType = 333,
+                    JsonContent = "some blog content here but really in json format",
+                }
+            };
+
+            var blogPostUploadResult = await client.Drive.UploadFile(FileSystemType.Standard, drive.TargetDriveInfo, blogMetadata, "some payload");
+
+            var commentMetadata = new UploadFileMetadata()
+            {
+                ReferencedFile = blogPostUploadResult.GlobalTransitIdFileIdentifier,
+                ContentType = "text/plain",
+                AppData = new UploadAppFileMetaData()
+                {
+                    FileType = 10101,
+                    JsonContent = "this is a comment about the blog post",
+                }
+            };
+
+            var commentUploadResult = await client.Drive.UploadFile(FileSystemType.Comment, drive.TargetDriveInfo, commentMetadata, "some payload");
+
+            var commentFileHeader = await client.Drive.GetFileHeader(FileSystemType.Comment, commentUploadResult.File);
+
+            Assert.IsTrue(commentFileHeader.ServerMetadata.FileSystemType == FileSystemType.Comment);
+            Assert.IsTrue(commentFileHeader.FileMetadata.AppData.JsonContent == commentMetadata.AppData.JsonContent);
+            Assert.IsTrue(commentFileHeader.FileMetadata.AppData.FileType == commentMetadata.AppData.FileType);
+
+            var blogPostHeaderWith1Comment = await client.Drive.GetFileHeader(FileSystemType.Standard, blogPostUploadResult.File);
+
+            Assert.IsNotNull(blogPostHeaderWith1Comment);
+            Assert.IsTrue(blogPostHeaderWith1Comment.FileMetadata.ReactionPreview.TotalCommentCount == 1);
+            
+            //
+            // Now delete the comment
+            //
+            await client.Drive.DeleteFile(commentUploadResult.File, null, FileSystemType.Comment);
+
+            var updatedBlogPostHeader = await client.Drive.GetFileHeader(FileSystemType.Standard, blogPostUploadResult.File);
+
+            Assert.IsNotNull(updatedBlogPostHeader);
+            Assert.IsTrue(updatedBlogPostHeader.FileMetadata.ReactionPreview.TotalCommentCount == 0);
+        }
 
         [Test]
         public async Task FailToGetCommentFromStandardFileSystem()
