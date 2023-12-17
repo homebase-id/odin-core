701fcbd3d456c36f8ee46fafaf527b60b4e602fb
Seifert69 <michael@corleone.com>
2023-12-17 16:25:47 +0100

    Cleaning up files in crypto a bit

--- lib/Odin.Core.Cryptography/Obsolete/YouAuthCrypto.cs
+++ lib/Odin.Core.Cryptography/Obsolete/YouAuthCrypto.cs
@@ -1,10 +1,12 @@
 using System;
 using Odin.Core.Cryptography.Data;
 
-namespace Odin.Core.Cryptography;
+namespace Odin.Core.Cryptography.Obsolete;
 
+[Obsolete]
 public class YouAuthCrypto
 {
+    [Obsolete]
     public static (EccFullKeyData keyPair, byte[] randomSalt) CreateEccKeyPair(SensitiveByteArray privateKey)
     {
         var keyPair = new EccFullKeyData(privateKey, EccKeySize.P384, 1);
