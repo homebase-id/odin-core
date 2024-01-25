54e5b58ba198317861bc879efbe8e8586cb0b1ff
Sebastian Bargmann <sebbarg@gmail.com>
2024-01-25 12:37:43 +0100

    fix drive race

--- services/Odin.Core.Services/AppNotifications/WebSocket/DeviceSocket.cs
+++ services/Odin.Core.Services/AppNotifications/WebSocket/DeviceSocket.cs
@@ -9,11 +9,11 @@ public class DeviceSocket
     public Guid Key { get; set; }
     public System.Net.WebSockets.WebSocket Socket { get; set; }
     public ClientAuthenticationToken DeviceAuthToken { get; set; }
-    
+
     /// <summary>
     /// List of drives to which this device socket is subscribed
     /// </summary>
-    public List<Guid> Drives { get; set; }
+    public List<Guid> Drives { get; set; } = [];
 
     public SensitiveByteArray SharedSecretKey { get; set; }
 }
