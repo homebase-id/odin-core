287a1ba100730022e36937e4b9ee949b4ec9655f
Seifert69 <michael@corleone.com>
2024-04-15 09:36:18 +0200

    undo todd code

--- tests/core/Odin.Core.Storage.Tests/IdentityDatabaseTests/DatabaseConnectionTests.cs
+++ tests/core/Odin.Core.Storage.Tests/IdentityDatabaseTests/DatabaseConnectionTests.cs
@@ -52,5 +52,46 @@ namespace Odin.Core.Storage.Tests.IdentityDatabaseTests
 
             }
         }
+
+
+        /// <summary>
+        /// Ensure that we can reuse prepared statments over two connections
+        /// </summary>
+        [Test]
+        public void DualConnectionPreparedStatementTest()
+        {
+            using var db = new IdentityDatabase("disco");
+
+            var k1 = Guid.NewGuid().ToByteArray();
+            var k2 = Guid.NewGuid().ToByteArray();
+            var k3 = Guid.NewGuid().ToByteArray();
+            var v1 = Guid.NewGuid().ToByteArray();
+            var v2 = Guid.NewGuid().ToByteArray();
+            var v3 = Guid.NewGuid().ToByteArray();
+
+            using (var myc = db.CreateDisposableConnection())
+            {
+                db.CreateDatabase(myc);
+
+                var r = db.tblKeyValue.Get(myc, k1);
+                Debug.Assert(r == null);
+
+                db.tblKeyValue.Insert(myc, new KeyValueRecord() { key = k1, data = v1 });
+                db.tblKeyValue.Insert(myc, new KeyValueRecord() { key = k2, data = v2 });
+
+                r = db.tblKeyValue.Get(myc, k1);
+                if (ByteArrayUtil.muidcmp(r.data, v1) != 0)
+                    Assert.Fail();
+            }
+
+            using (var myc2 = db.CreateDisposableConnection())
+            {
+                db.tblKeyValue.Insert(myc2, new KeyValueRecord() { key = k3, data = v3 });
+
+                var r = db.tblKeyValue.Get(myc2, k3);
+                if (ByteArrayUtil.muidcmp(r.data, v3) != 0)
+                    Assert.Fail();
+            }
+        }
     }
 }
