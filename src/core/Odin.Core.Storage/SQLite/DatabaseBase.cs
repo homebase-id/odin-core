10f98525037cf6fa4a886aa94f2cc7d5e46835f5
Seifert69 <michael@corleone.com>
2024-04-29 08:36:01 +0200

    Revert "clear connection"
    This reverts commit de1b41a7177a4d7155addc6f07ece8349eebb396.

--- src/core/Odin.Core.Storage/SQLite/DatabaseBase.cs
+++ src/core/Odin.Core.Storage/SQLite/DatabaseBase.cs
@@ -117,7 +117,6 @@ namespace Odin.Core.Storage.SQLite
             command.Transaction = connection._transaction;
             var r = command.ExecuteNonQuery();
             command.Transaction = null;
-            command.Connection = null;
             return r;
         }
 
@@ -130,7 +129,6 @@ namespace Odin.Core.Storage.SQLite
             command.Transaction = connection._transaction;
             var r = command.ExecuteReader();
             command.Transaction = null;
-            command.Connection = null;
             return r;
         }
 
