9e8aef54f6b68470102bf8ed1f5bc606bbb0ecea
Sebastian Bargmann <sebbarg@gmail.com>
2024-05-08 06:18:12 +0200

    Version cleanup

--- src/apps/Odin.Hosting/Controllers/Anonymous/VersionController.cs
+++ src/apps/Odin.Hosting/Controllers/Anonymous/VersionController.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using Odin.Hosting.Extensions;
 
 namespace Odin.Hosting.Controllers.Anonymous;
 
 public class VersionController
 {
     [HttpGet("/api/v1/version")]
-    public ActionResult<string> GetVersion(string filename)
+    public ActionResult<string> GetVersion()
     {
         return Version.VersionText;
     }
--- src/apps/Odin.Hosting/Extensions/Version.cs
+++ src/apps/Odin.Hosting/Extensions/Version.cs
@@ -1,6 +1,8 @@
-namespace Odin.Hosting;
+namespace Odin.Hosting.Extensions;
 
 // DO NOT CHANGE OR MOVE THIS FILE
+//
+// It is used to provide the version of the application and is patched during the build process
 
 public static class Version
 {
