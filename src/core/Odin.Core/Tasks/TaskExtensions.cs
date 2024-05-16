5620438b71aab33d8f3755ecf4747288ce992791
Sebastian Bargmann <sebbarg@gmail.com>
2024-05-16 09:32:14 +0200

    typo

--- src/core/Odin.Core/Tasks/TaskExtensions.cs
+++ src/core/Odin.Core/Tasks/TaskExtensions.cs
@@ -5,7 +5,7 @@ namespace Odin.Core.Tasks;
 public static class TaskExtensions
 {
     /// <summary>
-    /// Call this instead of Task.Wait() if you want want to deal with AggregateExceptions
+    /// Call Task.BlockingWait() instead of Task.Wait() if you don't want to deal with AggregateExceptions
     /// </summary>
     /// <param name="task"></param>
     public static void BlockingWait(this Task task)
