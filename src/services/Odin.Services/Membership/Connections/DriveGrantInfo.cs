222d1e39398165c9a449ab081f31fae3fa0f2eb4
todd mitchell <1205927+toddmitchell@users.noreply.github.com>
2024-05-21 16:22:56 -0700

    Empire bug fixes (#596)
    * removing dead sockets
    
    * removing dead sockets
    
    * adding troubleshooting feature
    
    * adding troubleshooting feature
    
    * adding troubleshooting feature
    
    ---------
    
    Co-authored-by: toddmitchell <t@lighttheory.co>

---
+++ src/services/Odin.Services/Membership/Connections/DriveGrantInfo.cs
@@ -0,0 +1,12 @@
+using Odin.Services.Drives;
+
+namespace Odin.Services.Membership.Connections;
+
+public class DriveGrantInfo
+{
+    public string DriveName { get; set; }
+    public bool DriveIsGranted { get; set; }
+    public DrivePermission ExpectedDrivePermission { get; set; }
+    public DrivePermission ActualDrivePermission { get; set; }
+    public int EncryptedKeyLength { get; set; }
+}
