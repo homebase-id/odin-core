f4d2afbfeb213a2dbb5f01990117ae5a0ec3cb88
Sebastian Bargmann <sebbarg@gmail.com>
2024-05-25 09:18:30 +0200

    New certs with root

--- src/apps/Odin.Hosting/https/get-certificates.sh
+++ src/apps/Odin.Hosting/https/get-certificates.sh
@@ -5,12 +5,12 @@ set -e
 rsync -v -r \
     --copy-links \
     --include '*/' \
-    --include 'fullchain.pem' \
+    --include 'fullchain-with-root.pem' \
     --include 'privkey.pem' \
     --exclude '*' \
     dns.id.pub:/letsencrypt-certificates/* ./
     
 # rename files
-find . -type f -name "fullchain.pem" -exec sh -c 'mv "$0" "${0%/*}/certificate.crt"' {} \;
+find . -type f -name "fullchain-with-root.pem" -exec sh -c 'mv "$0" "${0%/*}/certificate.crt"' {} \;
 find . -type f -name "privkey.pem" -exec sh -c 'mv "$0" "${0%/*}/private.key"' {} \;
 
