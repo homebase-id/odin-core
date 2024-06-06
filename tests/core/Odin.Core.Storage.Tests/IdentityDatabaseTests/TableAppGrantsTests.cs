ec0ddf1321eae0bf29b96faf82111e14e63c3835
Seifert69 <michael@corleone.com>
2024-06-06 10:40:58 +0200

    missing test case

--- tests/core/Odin.Core.Storage.Tests/IdentityDatabaseTests/TableAppGrantsTests.cs
+++ tests/core/Odin.Core.Storage.Tests/IdentityDatabaseTests/TableAppGrantsTests.cs
@@ -26,5 +26,28 @@ namespace Odin.Core.Storage.Tests.IdentityDatabaseTests
             }
         }
 
+
+        [Test]
+        public void TryInsertTest()
+        {
+            using var db = new IdentityDatabase(Guid.NewGuid(), "");
+
+            var c1 = SequentialGuid.CreateGuid();
+            var d1 = Guid.NewGuid().ToByteArray();
+            var c2 = SequentialGuid.CreateGuid();
+            var c3 = SequentialGuid.CreateGuid();
+            var d2 = Guid.NewGuid().ToByteArray();
+
+            using (var myc = db.CreateDisposableConnection())
+            {
+                db.CreateDatabase(myc);
+                var i = db.tblAppGrants.TryInsert(myc, new AppGrantsRecord() { appId = c1, circleId = c2, data = d1, odinHashId = c3 });
+                Debug.Assert(i == 1);
+
+                i = db.tblAppGrants.TryInsert(myc, new AppGrantsRecord() { appId = c1, circleId = c2, data = d1, odinHashId = c3 });
+                Debug.Assert(i == 0);
+            }
+        }
+
     }
 }
