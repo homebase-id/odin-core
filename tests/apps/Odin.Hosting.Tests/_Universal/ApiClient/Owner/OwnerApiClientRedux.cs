eb626f97dd9fa43be56363f68fa317d1a392693b
toddmitchell <t@lighttheory.co>
2024-06-26 11:29:43 -0700

    fixing tests

--- tests/apps/Odin.Hosting.Tests/_Universal/ApiClient/Owner/OwnerApiClientRedux.cs
+++ tests/apps/Odin.Hosting.Tests/_Universal/ApiClient/Owner/OwnerApiClientRedux.cs
@@ -7,12 +7,10 @@ using Odin.Hosting.Tests._Universal.ApiClient.Owner.AccountManagement;
 using Odin.Hosting.Tests._Universal.ApiClient.Owner.AppManagement;
 using Odin.Hosting.Tests._Universal.ApiClient.Owner.CircleMembership;
 using Odin.Hosting.Tests._Universal.ApiClient.Owner.Configuration;
-using Odin.Hosting.Tests._Universal.ApiClient.Owner.Cron;
 using Odin.Hosting.Tests._Universal.ApiClient.Owner.DriveManagement;
 using Odin.Hosting.Tests._Universal.ApiClient.Owner.YouAuth;
 using Odin.Hosting.Tests._Universal.ApiClient.Peer.Direct;
 using Odin.Hosting.Tests._Universal.ApiClient.Peer.Query;
-using Odin.Hosting.Tests.OwnerApi.ApiClient.Apps;
 using Odin.Hosting.Tests.OwnerApi.Utils;
 
 namespace Odin.Hosting.Tests._Universal.ApiClient.Owner
@@ -51,7 +49,6 @@ namespace Odin.Hosting.Tests._Universal.ApiClient.Owner
 
             AppNotifications = new AppNotificationsApiClient(identity.OdinId, factory);
 
-            Cron = new CronApiClient(ownerApi, identity);
             Connections = new CircleNetworkRequestsApiClient(ownerApi, identity);
 
             AccountManagement = new OwnerAccountManagementApiClient(ownerApi, identity);
@@ -64,9 +61,7 @@ namespace Odin.Hosting.Tests._Universal.ApiClient.Owner
         }
 
         public TestIdentity Identity => _identity;
-
-        public CronApiClient Cron { get; }
-
+        
         public UniversalFollowerApiClient Follower { get; }
 
         public UniversalDriveReactionClient Reactions { get; }
