ce2b0113ba08a22608cd2ce53ae7abe667a7d983
Sebastian Bargmann <sebbarg@gmail.com>
2024-06-27 16:11:51 +0200

    Add byte array key wrapper

--- src/core/Odin.Core/Cache/GenericMemoryCache.cs
+++ src/core/Odin.Core/Cache/GenericMemoryCache.cs
@@ -8,20 +8,34 @@ namespace Odin.Core.Cache;
 public interface IGenericMemoryCache
 {
     bool TryGet(string key, out object? value);
+    bool TryGet(byte[] key, out object? value);
     bool TryGet<T>(string key, out T? value);
+    bool TryGet<T>(byte[] key, out T? value);
     void Set(string key, object value, TimeSpan lifespan);
+    void Set(byte[] key, object value, TimeSpan lifespan);
     object? Remove(string key);
+    object? Remove(byte[] key);
     bool Contains(string key);
+    bool Contains(byte[] key);
 }
 
-public class GenericMemoryCache : IGenericMemoryCache
+public class GenericMemoryCache(string name = "generic-memory-cache") : IGenericMemoryCache
 {
-    private readonly MemoryCache _cache = new("generic-memory-cache");
+    private readonly MemoryCache _cache = new(name);
+
+    //
 
     public bool TryGet(string key, out object? value)
     {
         value = _cache.Get(key);
         return value != null;
+    }
+
+    //
+
+    public bool TryGet(byte[] key, out object? value)
+    {
+        return TryGet(Convert.ToBase64String(key), out value);
     }
 
     //
@@ -45,10 +59,24 @@ public class GenericMemoryCache : IGenericMemoryCache
 
     //
 
+    public bool TryGet<T>(byte[] key, out T? value)
+    {
+        return TryGet(Convert.ToBase64String(key), out value);
+    }
+
+    //
+
     public void Set(string key, object value, TimeSpan lifespan)
     {
         var policy = new CacheItemPolicy { AbsoluteExpiration = DateTimeOffset.Now.Add(lifespan) };
         _cache.Set(new CacheItem(key, value), policy);
+    }
+
+    //
+
+    public void Set(byte[] key, object value, TimeSpan lifespan)
+    {
+        Set(Convert.ToBase64String(key), value, lifespan);
     }
 
     //
@@ -60,8 +88,23 @@ public class GenericMemoryCache : IGenericMemoryCache
 
     //
 
+    public object? Remove(byte[] key)
+    {
+        return Remove(Convert.ToBase64String(key));
+    }
+
+    //
+
     public bool Contains(string key)
     {
         return _cache.Contains(key);
     }
+
+    //
+
+    public bool Contains(byte[] key)
+    {
+        return Contains(Convert.ToBase64String(key));
+    }
+
 }
--- tests/core/Odin.Core.Tests/Cache/GenericMemoryCacheTest.cs
+++ tests/core/Odin.Core.Tests/Cache/GenericMemoryCacheTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography;
 using System.Threading;
 using NUnit.Framework;
 using Odin.Core.Cache;
@@ -26,6 +27,22 @@ public class GenericMemoryCacheTest
             Assert.IsTrue(hit);
             Assert.AreEqual("bar", value!.Name);
         }
+
+        var key = RandomNumberGenerator.GetBytes(16);
+        cache.Set(key, "bar", TimeSpan.FromSeconds(10));
+        {
+            var hit = cache.TryGet(key, out var value);
+            Assert.IsTrue(hit);
+            Assert.AreEqual("bar", value);
+        }
+
+        cache.Set(key, new SampleValue(), TimeSpan.FromSeconds(10));
+        {
+            var hit = cache.TryGet<SampleValue>(key, out var value);
+            Assert.IsTrue(hit);
+            Assert.AreEqual("bar", value!.Name);
+        }
+
     }
 
     //
@@ -43,6 +60,14 @@ public class GenericMemoryCacheTest
             Assert.AreEqual("bar", value!.Name);
         }
 
+        var key = RandomNumberGenerator.GetBytes(16);
+        cache.Set(key, new SampleValue(), TimeSpan.FromMilliseconds(10));
+        {
+            var hit = cache.TryGet<SampleValue>(key, out var value);
+            Assert.IsTrue(hit);
+            Assert.AreEqual("bar", value!.Name);
+        }
+
         Thread.Sleep(200);
 
         {
@@ -50,6 +75,13 @@ public class GenericMemoryCacheTest
             Assert.IsFalse(hit);
             Assert.IsNull(value);
         }
+
+        {
+            var hit = cache.TryGet<SampleValue>(key, out var value);
+            Assert.IsFalse(hit);
+            Assert.IsNull(value);
+        }
+
     }
 
     //
@@ -60,16 +92,55 @@ public class GenericMemoryCacheTest
         // Arrange
         var cache = new GenericMemoryCache();
 
-        cache.Set("foo", new SampleValue(), TimeSpan.FromMilliseconds(100));
-
-        var exception = Assert.Throws<InvalidCastException>(() =>
         {
-            var hit = cache.TryGet<GenericMemoryCacheTest>("foo", out _);
-        });
-        Assert.AreEqual("The item with key 'foo' cannot be cast to type GenericMemoryCacheTest.", exception.Message);
+            cache.Set("foo", new SampleValue(), TimeSpan.FromMilliseconds(100));
+
+            var exception = Assert.Throws<InvalidCastException>(() =>
+            {
+                var hit = cache.TryGet<GenericMemoryCacheTest>("foo", out _);
+            });
+            Assert.AreEqual("The item with key 'foo' cannot be cast to type GenericMemoryCacheTest.", exception!.Message);
+        }
+
+        {
+            var key = RandomNumberGenerator.GetBytes(16);
+            cache.Set(key, new SampleValue(), TimeSpan.FromMilliseconds(100));
+
+            var exception = Assert.Throws<InvalidCastException>(() =>
+            {
+                var hit = cache.TryGet<GenericMemoryCacheTest>(key, out _);
+            });
+            Assert.AreEqual($"The item with key '{key.ToBase64()}' cannot be cast to type GenericMemoryCacheTest.", exception!.Message);
+        }
+
     }
 
     //
+
+    //
+
+    [Test]
+    public void ItShouldRemoveAnKey()
+    {
+        // Arrange
+        var cache = new GenericMemoryCache();
+
+        var key = RandomNumberGenerator.GetBytes(16);
+        cache.Set(key, "bar", TimeSpan.FromSeconds(10));
+        var hit = cache.TryGet(key, out var value);
+        Assert.IsTrue(hit);
+        Assert.AreEqual("bar", value);
+
+        var exists = cache.Contains(key);
+        Assert.IsTrue(exists);
+
+        value = cache.Remove(key);
+        Assert.AreEqual("bar", value);
+
+        exists = cache.Contains(key);
+        Assert.IsFalse(exists);
+    }
+
 
     private class SampleValue
     {
