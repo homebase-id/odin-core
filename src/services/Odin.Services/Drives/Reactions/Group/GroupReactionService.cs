d0e981a744892f874e5529a5a4ba0dbb82eefcb0
toddmitchell <t@lighttheory.co>
2024-08-01 11:29:30 -0700

    switched to using older reaction content service under the group reaction serivce

--- src/services/Odin.Services/Drives/Reactions/Group/GroupReactionService.cs
+++ src/services/Odin.Services/Drives/Reactions/Group/GroupReactionService.cs
@@ -1,23 +1,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using MediatR;
 using Odin.Core.Exceptions;
 using Odin.Core.Identity;
 using Odin.Core.Storage;
 using Odin.Core.Storage.SQLite;
-using Odin.Core.Time;
 using Odin.Services.Base;
-using Odin.Services.Drives.DriveCore.Query.Sqlite;
 using Odin.Services.Util;
-using Org.BouncyCastle.Asn1.Ocsp;
 
 namespace Odin.Services.Drives.Reactions.Group;
 
 public class GroupReactionService(
-    DriveDatabaseHost driveDatabaseHost,
-    IMediator mediator,
     TenantContext tenantContext,
+    ReactionContentService reactionContentService,
     FileSystemResolver fileSystemResolver)
 {
     public async Task<AddReactionResult> AddReaction(FileIdentifier fileId, string reaction, ReactionTransitOptions options, IOdinContext odinContext,
@@ -29,31 +24,14 @@ public class GroupReactionService(
         var file = await GetLocalFileId(fileId, odinContext, connection, fileSystemType);
 
         odinContext.PermissionsContext.AssertHasDrivePermission(file.DriveId, DrivePermission.React);
-        var callerId = odinContext.GetCallerOdinIdOrFail();
 
         var result = new AddReactionResult();
-        var manager = await driveDatabaseHost.TryGetOrLoadQueryManager(file.DriveId, connection);
-        if (manager != null)
+
+        await reactionContentService.AddReaction(file, reaction, odinContext, connection);
+
+        if (options?.Recipients?.Any() ?? false)
         {
-            manager.AddReaction(callerId, file.FileId, reaction, connection);
-
-            if (options?.Recipients?.Any() ?? false)
-            {
-                //TODO: enqueue in outbox and update result
-            }
-
-            await mediator.Publish(new ReactionContentAddedNotification
-            {
-                Reaction = new Reaction()
-                {
-                    OdinId = callerId,
-                    Created = UnixTimeUtcUnique.Now(), //TODO: i should technically pull this from the db records
-                    ReactionContent = reaction,
-                    FileId = file
-                },
-                OdinContext = odinContext,
-                DatabaseConnection = connection
-            });
+            //TODO: enqueue in outbox and update result
         }
 
         return result;
@@ -70,29 +48,11 @@ public class GroupReactionService(
         odinContext.PermissionsContext.AssertHasDrivePermission(file.DriveId, DrivePermission.React);
 
         var result = new DeleteReactionResult();
+        await reactionContentService.DeleteReaction(file, reaction, odinContext, connection);
 
-        var manager = await driveDatabaseHost.TryGetOrLoadQueryManager(file.DriveId, connection);
-        if (manager != null)
+        if (options?.Recipients?.Any() ?? false)
         {
-            manager.DeleteReaction(odinContext.GetCallerOdinIdOrFail(), file.FileId, reaction, connection);
-
-            if (options?.Recipients?.Any() ?? false)
-            {
-                //TODO: enqueue in outbox and update result
-            }
-
-            await mediator.Publish(new ReactionDeletedNotification
-            {
-                Reaction = new Reaction()
-                {
-                    OdinId = odinContext.GetCallerOdinIdOrFail(),
-                    Created = default,
-                    ReactionContent = reaction,
-                    FileId = file
-                },
-                OdinContext = odinContext,
-                DatabaseConnection = connection
-            });
+            //TODO: enqueue in outbox and update result
         }
 
         return result;
@@ -105,19 +65,7 @@ public class GroupReactionService(
 
         odinContext.PermissionsContext.AssertHasDrivePermission(file.DriveId, DrivePermission.Read);
 
-        var manager = await driveDatabaseHost.TryGetOrLoadQueryManager(file.DriveId, connection);
-        if (manager != null)
-        {
-            var (reactions, total) = manager.GetReactionSummaryByFile(file.FileId, connection);
-
-            return new GetReactionCountsResponse()
-            {
-                Reactions = reactions,
-                Total = total
-            };
-        }
-
-        throw new OdinSystemException($"Invalid query manager instance for drive {file.DriveId}");
+        return await reactionContentService.GetReactionCountsByFile(file, odinContext, connection);
     }
 
     public async Task<List<string>> GetReactionsByIdentityAndFile(OdinId identity, FileIdentifier fileId, IOdinContext odinContext,
@@ -129,14 +77,7 @@ public class GroupReactionService(
 
         odinContext.PermissionsContext.AssertHasDrivePermission(file.DriveId, DrivePermission.Read);
 
-        var manager = await driveDatabaseHost.TryGetOrLoadQueryManager(file.DriveId, connection);
-        if (manager != null)
-        {
-            var result = manager.GetReactionsByIdentityAndFile(identity, file.FileId, connection);
-            return result;
-        }
-
-        throw new OdinSystemException($"Invalid query manager instance for drive {file.DriveId}");
+        return await reactionContentService.GetReactionsByIdentityAndFile(identity, file, odinContext, connection);
     }
 
     public async Task<GetReactionsResponse> GetReactions(FileIdentifier fileId, int cursor, int maxCount, IOdinContext odinContext,
@@ -145,21 +86,8 @@ public class GroupReactionService(
         var file = await GetLocalFileId(fileId, odinContext, connection, fileSystemType);
 
         odinContext.PermissionsContext.AssertHasDrivePermission(file.DriveId, DrivePermission.Read);
-
-        var manager = await driveDatabaseHost.TryGetOrLoadQueryManager(file.DriveId, connection);
-        if (manager != null)
-        {
-            var (list, nextCursor) =
-                manager.GetReactionsByFile(maxCount, cursor, file.FileId, connection);
-
-            return new GetReactionsResponse()
-            {
-                Reactions = list,
-                Cursor = nextCursor
-            };
-        }
-
-        throw new OdinSystemException($"Invalid query manager instance for drive {file.DriveId}");
+        
+        return await reactionContentService.GetReactions(file, cursor, maxCount, odinContext, connection);
     }
 
     //
