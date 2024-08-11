d96e97a42c64af2658e502e43e5c664f27753217
Seifert69 <michael@corleone.com>
2024-08-11 13:52:16 +0200

    Signature Example for Todd for Introductions

--- tests/core/Odin.Core.Cryptography.Tests/TestSignatureData.cs
+++ tests/core/Odin.Core.Cryptography.Tests/TestSignatureData.cs
@@ -10,6 +10,40 @@ using System;
 [TestFixture]
 public class SignatureDataTest
 {
+    /// <summary>
+    /// Here's an example Todd of how to sign a request. In the example the string testData is a JSON string
+    /// (probably incorrectly formatted) that contains the two parties to be introduced. Frodo is the signatory.
+    /// You end up with a singature data-structure that contains who signed it, what their public key was
+    /// (you should still validate it), when it was signed. 
+    /// Then it calls "verify" with the exact same JSON string, and if it is the same string, the it passes.
+    /// 
+    /// What you should be careful of, is making sure that you don't leave room in your code for someone 
+    /// signing anything other than an introduction. if that might happen then you should pre-pend your string
+    /// with a constant string to ensure that your function cannot sign "other" documents. For example if you 
+    /// pre-pend "introduction_signature:" to the string, then only that particular string will validate.
+    /// 
+    /// As part of transmitting the introduction request you would need to send the exact JSON string as well
+    /// as the signature to both parties.
+    /// </summary>
+    [Test]
+    public void Example_Introduction_Signature()
+    {
+        // Arrange
+        byte[] testData = System.Text.Encoding.UTF8.GetBytes("introducing: { partyA : \"sam.gamgee.me\", partyB: \"pippin.me\" }");
+        OdinId testIdentity = new OdinId("frodo.baggins.me");
+        SensitiveByteArray testKeyPwd = new SensitiveByteArray(Guid.NewGuid().ToByteArray());
+        EccFullKeyData testEccKey = new EccFullKeyData(testKeyPwd, EccKeySize.P384, 1);
+
+        // Act
+        SignatureData signature = SignatureData.NewSignature(testData, testIdentity, testKeyPwd, testEccKey);
+        Assert.GreaterOrEqual(signature.Signature.Length, 16);
+        bool isValid = SignatureData.Verify(signature, testData);
+
+        // Assert
+        Assert.IsTrue(isValid);
+    }
+
+
     [Test]
     public void Sign_And_Verify_Valid_Data_Should_Return_True()
     {
