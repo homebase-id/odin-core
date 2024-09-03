3946a15d1d3bf791a5a0aa9da75223faba398201
toddmitchell <t@lighttheory.co>
2024-09-03 08:02:52 -0700

    added explicit payload and thumbnail content type; pair programming with @stef

--- tests/apps/Odin.Hosting.Tests/_Universal/DriveTests/DirectDrivePayloadTests_1.cs
+++ tests/apps/Odin.Hosting.Tests/_Universal/DriveTests/DirectDrivePayloadTests_1.cs
@@ -132,6 +132,77 @@ public class DirectDrivePayloadTests_1
 
     [Test]
     [TestCaseSource(nameof(TestCases))]
+    public async Task CanSetExplicitContentTypeOnPayload(IApiClientContext callerContext, HttpStatusCode expectedStatusCode)
+    {
+        var identity = TestIdentities.Samwise;
+        var ownerApiClient = _scaffold.CreateOwnerApiClientRedux(identity);
+
+        var targetDrive = callerContext.TargetDrive;
+        await ownerApiClient.DriveManager.CreateDrive(callerContext.TargetDrive, "Test Drive 001", "", allowAnonymousReads: true);
+
+        var uploadedFileMetadata = SampleMetadataData.Create(fileType: 100);
+
+        var uploadedPayloadDefinition = SamplePayloadDefinitions.GetPayloadDefinitionWithThumbnail1();
+        var testPayloads = new List<TestPayloadDefinition>() { uploadedPayloadDefinition };
+
+        var uploadManifest = new UploadManifest()
+        {
+            PayloadDescriptors = testPayloads.ToPayloadDescriptorList().ToList()
+        };
+
+        var response = await ownerApiClient.DriveRedux.UploadNewFile(targetDrive, uploadedFileMetadata, uploadManifest, testPayloads);
+
+        Assert.IsTrue(response.IsSuccessStatusCode);
+        var uploadResult = response.Content;
+        Assert.IsNotNull(uploadResult);
+
+        // get the file header
+        var getHeaderResponse = await ownerApiClient.DriveRedux.GetFileHeader(uploadResult.File);
+        Assert.IsTrue(getHeaderResponse.IsSuccessStatusCode);
+        var header = getHeaderResponse.Content;
+        Assert.IsNotNull(header);
+        Assert.IsTrue(header.FileMetadata.Payloads.Count() == 1);
+
+        var payloadFromHeader = header.FileMetadata.GetPayloadDescriptor(uploadedPayloadDefinition.Key);
+        Assert.IsNotNull(payloadFromHeader, "payload not found in header");
+        Assert.IsTrue(ByteArrayUtil.EquiByteArrayCompare(payloadFromHeader.Iv, uploadedPayloadDefinition.Iv));
+
+        await callerContext.Initialize(ownerApiClient);
+        var uniDriveClient = new UniversalDriveApiClient(identity.OdinId, callerContext.GetFactory());
+
+        // Get the payload and check the headers
+        var getPayloadKey1Response = await uniDriveClient.GetPayload(uploadResult.File, uploadedPayloadDefinition.Key);
+
+        Assert.IsTrue(getPayloadKey1Response.StatusCode == expectedStatusCode);
+
+        if (expectedStatusCode == HttpStatusCode.OK) //test more
+        {
+            Assert.IsNotNull(getPayloadKey1Response.ContentHeaders);
+            Assert.IsNotNull(getPayloadKey1Response.Headers);
+
+            Assert.IsTrue(getPayloadKey1Response.Headers.TryGetValues(HttpHeaderConstants.PayloadEncrypted, out var isEncryptedValues));
+            Assert.IsFalse(bool.Parse(isEncryptedValues.Single()));
+
+            Assert.IsTrue(getPayloadKey1Response.Headers.TryGetValues(HttpHeaderConstants.PayloadKey, out var payloadKeyValues));
+            Assert.IsTrue(payloadKeyValues.Single() == uploadedPayloadDefinition.Key);
+            Assert.IsTrue(getPayloadKey1Response.Headers.TryGetValues(HttpHeaderConstants.DecryptedContentType, out var contentTypeValues));
+            Assert.IsTrue(contentTypeValues.Single() == uploadedPayloadDefinition.ContentType);
+
+            Assert.IsTrue(getPayloadKey1Response.Headers.TryGetValues(HttpHeaderConstants.SharedSecretEncryptedHeader64, out var encryptedHeader64Values));
+
+            var payloadEkh = EncryptedKeyHeader.FromBase64(encryptedHeader64Values.Single());
+            Assert.IsNotNull(payloadEkh);
+            Assert.IsTrue(ByteArrayUtil.EquiByteArrayCompare(payloadEkh.Iv, uploadedPayloadDefinition.Iv));
+            Assert.IsTrue(ByteArrayUtil.EquiByteArrayCompare(payloadEkh.EncryptedAesKey, header.SharedSecretEncryptedKeyHeader.EncryptedAesKey));
+
+            Assert.IsTrue(DriveFileUtility.TryParseLastModifiedHeader(getPayloadKey1Response.ContentHeaders, out var lastModifiedHeaderValue));
+            //Note commented as I'm having some conversion issues i think
+            Assert.IsTrue(lastModifiedHeaderValue.GetValueOrDefault().seconds == payloadFromHeader.LastModified.seconds);
+        }
+    }
+    
+    [Test]
+    [TestCaseSource(nameof(TestCases))]
     public async Task CanModifyPayloadOnExistingFileAndMetadataIsAutomaticallyUpdated(IApiClientContext callerContext, HttpStatusCode expectedStatusCode)
     {
         var identity = TestIdentities.Pippin;
