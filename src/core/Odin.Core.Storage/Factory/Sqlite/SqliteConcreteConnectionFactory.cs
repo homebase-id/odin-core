e14e281831677677433c8a7b50e1d682159efb34
Sebastian Bargmann <sebbarg@gmail.com>
2024-11-28 11:43:08 +0100

    SqliteConcreteConnectionFactory: fix pragma bug

--- src/core/Odin.Core.Storage/Factory/Sqlite/SqliteConcreteConnectionFactory.cs
+++ src/core/Odin.Core.Storage/Factory/Sqlite/SqliteConcreteConnectionFactory.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Data.Common;
 using System.Threading.Tasks;
 using Microsoft.Data.Sqlite;
@@ -8,20 +9,20 @@ namespace Odin.Core.Storage.Factory.Sqlite;
 internal static class SqliteConcreteConnectionFactory
 {
     private static readonly AsyncLock Mutex = new ();
-    private static bool _initialized;
+    private static readonly HashSet<string> PragmasExecuted = [];
 
     internal static async Task<DbConnection> Create(string connectionString)
     {
         // SEB:TODO do we need explicit retry logic here?
         var connection = new SqliteConnection(connectionString);
         await connection.OpenAsync();
-        if (!_initialized)
+
+        if (!PragmasExecuted.Contains(connectionString))
         {
             using (await Mutex.LockAsync())
             {
-                if (!_initialized)
+                if (PragmasExecuted.Add(connectionString))
                 {
-                    _initialized = true;
                     await using var command = connection.CreateCommand();
                     command.CommandText = "PRAGMA journal_mode=WAL;";
                     await command.ExecuteNonQueryAsync();
