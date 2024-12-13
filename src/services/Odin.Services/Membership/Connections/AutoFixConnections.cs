c310417ea58984d9990e05d9794bc55143ff8021
Sebastian Bargmann <sebbarg@gmail.com>
2024-12-13 09:45:13 +0100

    fix transactions: ConnectionAutoFixService

--- src/services/Odin.Services/Membership/Connections/AutoFixConnections.cs
+++ src/services/Odin.Services/Membership/Connections/AutoFixConnections.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Odin.Core.Storage.Database.Identity;
 using Odin.Core.Storage.SQLite;
 using Odin.Services.Authorization.Apps;
 using Odin.Services.Base;
@@ -14,28 +15,30 @@ namespace Odin.Services.Membership.Connections
         ILogger<ConnectionAutoFixService> logger,
         IAppRegistrationService appRegistrationService,
         CircleDefinitionService circleDefinitionService,
-        CircleNetworkService circleNetworkService)
+        CircleNetworkService circleNetworkService,
+        IdentityDatabase db)
     {
         public async Task AutoFixAsync(IOdinContext odinContext)
         {
             odinContext.Caller.AssertHasMasterKey();
+
+            await using var tx = await db.BeginStackedTransactionAsync();
+
             var allIdentities = await circleNetworkService.GetConnectedIdentitiesAsync(int.MaxValue, 0, odinContext);
-            
-            // TODO CONNECTIONS
-            // await cn.CreateCommitUnitOfWorkAsync(async () =>
-            // {
-                foreach (var identity in allIdentities.Results)
-                {
-                    await FixIdentityAsync(identity, odinContext);
-                }
-            
-                var allApps = await appRegistrationService.GetRegisteredAppsAsync(odinContext);
-                foreach (var app in allApps)
-                {
-                    logger.LogDebug("Calling ReconcileAuthorizedCircles for app {appName}", app.Name);
-                    await circleNetworkService.ReconcileAuthorizedCircles(oldAppRegistration: null, app, odinContext);
-                }
-            // });
+
+            foreach (var identity in allIdentities.Results)
+            {
+                await FixIdentityAsync(identity, odinContext);
+            }
+
+            var allApps = await appRegistrationService.GetRegisteredAppsAsync(odinContext);
+            foreach (var app in allApps)
+            {
+                logger.LogDebug("Calling ReconcileAuthorizedCircles for app {appName}", app.Name);
+                await circleNetworkService.ReconcileAuthorizedCircles(oldAppRegistration: null, app, odinContext);
+            }
+
+            tx.Commit();
         }
 
         private async Task FixIdentityAsync(IdentityConnectionRegistration icr, IOdinContext odinContext)
