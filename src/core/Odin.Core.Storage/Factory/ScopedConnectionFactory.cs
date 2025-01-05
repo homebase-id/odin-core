6eaf7b346efef24fc08e9e4dbbd79e4e5dd5eeda
Sebastian Bargmann <sebbarg@gmail.com>
2025-01-05 14:51:07 +0100

    Update NoParallelism comment

--- src/core/Odin.Core.Storage/Factory/ScopedConnectionFactory.cs
+++ src/core/Odin.Core.Storage/Factory/ScopedConnectionFactory.cs
@@ -259,7 +259,7 @@ public class ScopedConnectionFactory<T>(
     //
 
     // SEB:NOTE
-    // This method is used to detect parallelism, i.e. if the same connection is used in multiple threads.
+    // This method is used to detect parallelism, i.e. if the same instance of this claass is used in multiple threads.
     // We used to have locking all around this class, but while that might save you when (mis)using the class across
     // different threads or parallel tasks, it would not alert you to the fact that you are doing something wrong.
     // Since we already know we're not thread-safe, we might as well make it explicit and throw an exception instead
