472969f1bf3cb9f6e6175ec65fa429205de83b1d
toddmitchell <t@lighttheory.co>
2025-01-06 08:55:53 -0600

    updating upgrade process

--- src/services/Odin.Services/Configuration/VersionUpgrade/Version2tov3/V2ToV3VersionMigrationService.cs
+++ src/services/Odin.Services/Configuration/VersionUpgrade/Version2tov3/V2ToV3VersionMigrationService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,7 +8,6 @@ using Odin.Core.Exceptions;
 using Odin.Core.Identity;
 using Odin.Core.Time;
 using Odin.Services.Base;
-using Odin.Services.Membership.Connections;
 using Odin.Services.Membership.Connections.Requests;
 
 namespace Odin.Services.Configuration.VersionUpgrade.Version2tov3
@@ -19,9 +17,10 @@ namespace Odin.Services.Configuration.VersionUpgrade.Version2tov3
     /// </summary>
     public class V2ToV3VersionMigrationService(
         ILogger<V2ToV3VersionMigrationService> logger,
+        CircleNetworkRequestService circleNetworkRequestService,
         CircleNetworkIntroductionService introductionService)
     {
-        private readonly UnixTimeUtc _maxDate = UnixTimeUtc.FromDateTime(new DateTime(2025, 1, 5));
+        private readonly UnixTimeUtc _maxDate = UnixTimeUtc.FromDateTime(new DateTime(2025, 1, 6));
 
         public async Task UpgradeAsync(IOdinContext odinContext, CancellationToken cancellationToken)
         {
@@ -32,14 +31,72 @@ namespace Odin.Services.Configuration.VersionUpgrade.Version2tov3
             //
             logger.LogDebug("Deleting all introductions before {maxDate}", _maxDate.ToDateTime().ToShortDateString());
             await introductionService.DeleteIntroductionsAsync(odinContext, _maxDate);
+
+            //
+            // delete all outgoing connection requests that were due to an introduction
+            //
+            var outgoingRequests = await circleNetworkRequestService.GetSentRequestsAsync(PageOptions.All, odinContext);
+            var introductoryOutgoingRequests = outgoingRequests.Results.Where(o =>
+                o.ConnectionRequestOrigin == ConnectionRequestOrigin.Introduction &&
+                o.ReceivedTimestampMilliseconds < _maxDate);
+            foreach (var request in introductoryOutgoingRequests)
+            {
+                await circleNetworkRequestService.DeleteSentRequest((OdinId)request.Recipient, odinContext);
+            }
+
+            //
+            // delete all incoming connection requests that were due to an introduction
+            //
+            var incomingRequests = await circleNetworkRequestService.GetPendingRequestsAsync(PageOptions.All, odinContext);
+            foreach (var r in incomingRequests.Results.Where(i => i.ReceivedTimestampMilliseconds < _maxDate))
+            {
+                try
+                {
+                    // Note: it could fail to decrypt given the change in the key type, so we capture just in case
+                    var incomingRequest = await circleNetworkRequestService.GetPendingRequestAsync(r.SenderOdinId, odinContext);
+                    if (incomingRequest.ConnectionRequestOrigin == ConnectionRequestOrigin.Introduction)
+                    {
+                        await circleNetworkRequestService.DeletePendingRequest(r.SenderOdinId, odinContext);
+                    }
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, "Failed to delete introductory request from {sender}", r.SenderOdinId);
+                }
+            }
         }
 
         public async Task ValidateUpgradeAsync(IOdinContext odinContext, CancellationToken cancellationToken)
         {
             var introductions = await introductionService.GetReceivedIntroductionsAsync(odinContext);
-            if (introductions.Any(intro => intro.Received > _maxDate))
+            if (introductions.Any(intro => intro.Received < _maxDate))
             {
-                throw new OdinSystemException($"Validation failed: there is one or more introductions after {_maxDate}");
+                throw new OdinSystemException($"Validation failed: there is one or more introductions before {_maxDate.ToDateTime().ToShortDateString()}");
+            }
+
+            var outgoingRequests = await circleNetworkRequestService.GetSentRequestsAsync(PageOptions.All, odinContext);
+            var hasOutgoingIntroductory =
+                outgoingRequests.Results.Any(o => o.ConnectionRequestOrigin == ConnectionRequestOrigin.Introduction);
+            if (hasOutgoingIntroductory)
+            {
+                throw new OdinSystemException($"Validation failed: there is one or more outgoing-introductory requests before {_maxDate.ToDateTime().ToShortDateString()}");
+            }
+
+            var incomingRequests = await circleNetworkRequestService.GetPendingRequestsAsync(PageOptions.All, odinContext);
+            foreach (var r in incomingRequests.Results.Where(i => i.ReceivedTimestampMilliseconds < _maxDate))
+            {
+                try
+                {
+                    var incomingRequest = await circleNetworkRequestService.GetPendingRequestAsync(r.SenderOdinId, odinContext);
+                    if (incomingRequest.ConnectionRequestOrigin == ConnectionRequestOrigin.Introduction)
+                    {
+                        throw new OdinSystemException($"Validation failed: there is one or more incoming-introductory requests before {_maxDate.ToDateTime().ToShortDateString()}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, "Failed to delete introductory request from {sender}", r.SenderOdinId);
+                }
             }
         }
     }
