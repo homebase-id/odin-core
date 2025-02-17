d59fa0f6b7cf20de4e69bf2c29226d00ae7c2cb3
Sebastian Bargmann <sebbarg@gmail.com>
2025-02-17 07:48:31 +0100

    copy dev certs to FE after update

--- src/apps/Odin.Hosting/https/get-certificates.sh
+++ src/apps/Odin.Hosting/https/get-certificates.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-#  copy certificates from certbot location
+#
+# copy certificates from certbot location
+#
 rsync -v -r \
     --copy-links \
     --include '*/' \
@@ -9,8 +11,25 @@ rsync -v -r \
     --include 'privkey.pem' \
     --exclude '*' \
     dns.id.pub:/letsencrypt-certificates/* ./
-    
+
+#
 # rename files
+#
 find . -type f -name "fullchain.pem" -exec sh -c 'mv "$0" "${0%/*}/certificate.crt"' {} \;
 find . -type f -name "privkey.pem" -exec sh -c 'mv "$0" "${0%/*}/private.key"' {} \;
 
+#
+# copy to front end
+#
+TARGET_DIR="../../../../../odin-js"
+
+if [ -d "$TARGET_DIR" ]; then
+    cp ./dev.dotyou.cloud/certificate.crt "$TARGET_DIR/dev-dotyou-cloud.crt"
+    cp ./dev.dotyou.cloud/private.key "$TARGET_DIR/dev-dotyou-cloud.key"
+
+    if [ -d "$TARGET_DIR/packages/apps/login-app" ]; then
+        cp -r ./anon.dotyou.cloud "$TARGET_DIR/packages/apps/login-app"
+    fi
+else
+    echo "Front end directory not found: $TARGET_DIR. You must copy the certificates manually."
+fi
