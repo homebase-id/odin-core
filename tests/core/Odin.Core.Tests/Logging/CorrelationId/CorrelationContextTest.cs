4f244dd81de3691779d65f687420ce3d26fdcf4b
Sebastian Bargmann <sebbarg@gmail.com>
2025-02-19 07:43:40 +0100

    Outbox correlation processing

--- tests/core/Odin.Core.Tests/Logging/CorrelationId/CorrelationContextTest.cs
+++ tests/core/Odin.Core.Tests/Logging/CorrelationId/CorrelationContextTest.cs
@@ -1,71 +1,117 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Odin.Core.Logging.CorrelationId;
 
-namespace Odin.Core.Tests.Logging.CorrelationId
+namespace Odin.Core.Tests.Logging.CorrelationId;
+
+[TestFixture]
+public class CorrelationContextTest
 {
-    [TestFixture]
-    public class CorrelationContextTest
+    [Test]
+    public async Task CanCreateNewIdAcrossAsyncContexts1()
     {
-        [Test(Description = "Can create new id across async contexts")]
-        public async Task CanCreateNewIdAcrossAsyncContexts()
+        var id1 = "";
+        var id2 = "";
+
+        var tasks = new Task[]
         {
-            var id1 = "";
-            var id2 = "";
-
-            var tasks = new Task[]
+            Task.Run(() =>
             {
-                Task.Run(() =>
+                var cc1 = new CorrelationContext(new CorrelationUniqueIdGenerator());
+                Assert.False(string.IsNullOrEmpty(cc1.Id));
+                id1 = cc1.Id;
+            }),
+            Task.Run(() =>
+            {
+                var cc2 = new CorrelationContext(new CorrelationUniqueIdGenerator());
+                Assert.False(string.IsNullOrEmpty(cc2.Id));
+                id2 = cc2.Id;
+
+                var thread = new Thread(() =>
                 {
-                    var cc1 = new CorrelationContext(new CorrelationUniqueIdGenerator());
-                    Assert.False(string.IsNullOrEmpty(cc1.Id));
-                    id1 = cc1.Id;
-                }),
-                Task.Run(() =>
-                {
-                    var cc2 = new CorrelationContext(new CorrelationUniqueIdGenerator());
-                    Assert.False(string.IsNullOrEmpty(cc2.Id));
-                    id2 = cc2.Id;
-                    
-                    var thread = new Thread(() => 
+                    var cc3 = new CorrelationContext(new CorrelationUniqueIdGenerator());
+                    Assert.False(string.IsNullOrEmpty(cc3.Id));
+                    Assert.AreEqual(id2, cc3.Id);
+
+                    Task.Run(() =>
                     {
-                        var cc3 = new CorrelationContext(new CorrelationUniqueIdGenerator());
-                        Assert.False(string.IsNullOrEmpty(cc3.Id));
-                        Assert.AreEqual(id2, cc3.Id);
-                    
-                        Task.Run(() =>
-                        {
-                            var cc4 = new CorrelationContext(new CorrelationUniqueIdGenerator());
-                            Assert.False(string.IsNullOrEmpty(cc4.Id));
-                            Assert.AreEqual(id2, cc4.Id);
-                        }).Wait();
-                        
-                        var flow = ExecutionContext.SuppressFlow();
-                        Task.Run(() =>
-                        {
-                            var cc5 = new CorrelationContext(new CorrelationUniqueIdGenerator());
-                            Assert.False(string.IsNullOrEmpty(cc5.Id));
-                            Assert.AreNotEqual(id2, cc5.Id);
-                        }).Wait();
-                        flow.Undo();
+                        var cc4 = new CorrelationContext(new CorrelationUniqueIdGenerator());
+                        Assert.False(string.IsNullOrEmpty(cc4.Id));
+                        Assert.AreEqual(id2, cc4.Id);
+                    }).Wait();
 
-                        Task.Run(() =>
-                        {
-                            var cc6 = new CorrelationContext(new CorrelationUniqueIdGenerator());
-                            Assert.False(string.IsNullOrEmpty(cc6.Id));
-                            Assert.AreEqual(id2, cc6.Id);
-                        }).Wait();
+                    var flow = ExecutionContext.SuppressFlow();
+                    Task.Run(() =>
+                    {
+                        var cc5 = new CorrelationContext(new CorrelationUniqueIdGenerator());
+                        Assert.False(string.IsNullOrEmpty(cc5.Id));
+                        Assert.AreNotEqual(id2, cc5.Id);
+                    }).Wait();
+                    flow.Undo();
 
-                    });
-                    thread.Start();
-                    thread.Join();
-                })
+                    Task.Run(() =>
+                    {
+                        var cc6 = new CorrelationContext(new CorrelationUniqueIdGenerator());
+                        Assert.False(string.IsNullOrEmpty(cc6.Id));
+                        Assert.AreEqual(id2, cc6.Id);
+                    }).Wait();
+
+                });
+                thread.Start();
+                thread.Join();
+            })
+        };
+
+        await Task.WhenAll(tasks);
+        Assert.AreNotEqual(id1, id2);
+    }
+
+    //
+
+    [Test]
+    public async Task CanCreateNewIdAcrossAsyncContexts2()
+    {
+        var id1 = "";
+        var id2 = "";
+
+        var cc0 = new CorrelationContext(new CorrelationUniqueIdGenerator())
+        {
+            Id = "000"
+        };
+
+        var tasks = new List<Task>
+        {
+            CreateNewIdAsync1("111"),
+            CreateNewIdAsync2("222")
+        };
+
+        await Task.WhenAll(tasks);
+        Assert.AreEqual(cc0.Id, "000");
+        Assert.AreEqual(id1, "111");
+        Assert.AreEqual(id2, "222");
+
+        return;
+
+        async Task CreateNewIdAsync1(string s)
+        {
+            var cc = new CorrelationContext(new CorrelationUniqueIdGenerator())
+            {
+                Id = s
             };
-
-            await Task.WhenAll(tasks);
-            Assert.AreNotEqual(id1, id2);
+            await Task.Delay(100);
+            id1 = cc.Id;
         }
 
+        async Task CreateNewIdAsync2(string s)
+        {
+            var cc = new CorrelationContext(new CorrelationUniqueIdGenerator())
+            {
+                Id = s
+            };
+            await Task.Delay(10);
+            id2 = cc.Id;
+        }
     }
 }
