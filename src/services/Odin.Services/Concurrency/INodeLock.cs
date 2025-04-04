405d2059110084fe4ec5685f58f728ecbe38aad4
Sebastian Bargmann <sebbarg@gmail.com>
2025-04-04 10:38:03 +0200

    Add NodeLock and RedisLock (#984)
    * Add NodeLock and RedisLock
    
    * Fix windows tests

---
+++ src/services/Odin.Services/Concurrency/INodeLock.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Odin.Services.Concurrency;
+
+#nullable enable
+
+public interface INodeLock
+{
+    public Task<IAsyncDisposable> LockAsync(
+        string key,
+        TimeSpan? timeout = null,         // Timeout after timespan. Only used for distributed locks.
+        TimeSpan? forcedRelease = null,   // Force release lock after timespan. Only used for distributed locks.
+        CancellationToken cancellationToken = default);
+}
---
+++ src/services/Odin.Services/Concurrency/NodeLock.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Odin.Core.Threading;
+
+namespace Odin.Services.Concurrency;
+
+#nullable enable
+
+public sealed class NodeLock : INodeLock
+{
+    private readonly KeyedAsyncLock _lock = new ();
+
+    public async Task<IAsyncDisposable> LockAsync(
+        string key,
+        TimeSpan? timeout = null,        // ignored in this lock
+        TimeSpan? forcedRelease = null,  // ignored in this lock
+        CancellationToken cancellationToken = default)
+    {
+        var disposable = await _lock.LockAsync(key, cancellationToken);
+        return new Releaser(disposable);
+    }
+
+    private sealed class Releaser(IDisposable disposable) : IAsyncDisposable
+    {
+        public ValueTask DisposeAsync()
+        {
+            disposable.Dispose();
+            return ValueTask.CompletedTask;
+        }
+    }
+}
