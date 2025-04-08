676f7e6219d1accbb9753d6ef4323033630b9e07
Sebastian Bargmann <sebbarg@gmail.com>
2025-04-08 07:41:55 +0200

    Logging: log all inner exception messages on first line for a happy log parser

--- src/core/Odin.Core/Logging/Exception/Serilog/ExceptionEnricher.cs
+++ src/core/Odin.Core/Logging/Exception/Serilog/ExceptionEnricher.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Serilog;
 using Serilog.Configuration;
 using Serilog.Core;
@@ -9,17 +10,30 @@ public class ExceptionMessageEnricher : ILogEventEnricher
 {
     public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
     {
-        if (logEvent.Exception != null)
+        if (logEvent.Exception == null)
         {
-            var exceptionMessage = logEvent.Exception.Message;
-            var logMessage = logEvent.RenderMessage();
-
-            if (!logMessage.Contains(exceptionMessage))
-            {
-                logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
-                    "ExceptionMessage", $" ({logEvent.Exception.GetType().Name}: {exceptionMessage})"));
-            }
+            return;
         }
+
+        var exception = logEvent.Exception;
+        var logMessage = logEvent.RenderMessage();
+
+        // We assume that if the top exception message is already in the log message,
+        // the user is not interested in the rest of the exception messages.
+        if (logMessage.Contains(exception.Message))
+        {
+            return;
+        }
+
+        var errors = new List<string>();
+        while (exception != null)
+        {
+            errors.Add(exception.Message.Replace("\r\n", " ").Replace("\r", " ").Replace("\n", " "));
+            exception = exception.InnerException;
+        }
+
+        logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+            "ExceptionMessage", $" ({string.Join(" -> ", errors)})"));
     }
 }
 
