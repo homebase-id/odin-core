833f69f37f5aa77b8063319fa5732a86ae8a6b59
toddmitchell <t@lighttheory.co>
2025-04-09 17:51:09 -0700

    ported over version upgrade code for simplicity

---
+++ src/services/Odin.Services/Configuration/VersionUpgrade/Version3tov4/V3ToV4VersionMigrationService.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Odin.Core.Exceptions;
+using Odin.Services.Base;
+using Odin.Services.Membership.Circles;
+using Odin.Services.Membership.Connections;
+
+namespace Odin.Services.Configuration.VersionUpgrade.Version3tov4
+{
+    /// <summary>
+    /// Service to handle converting data between releases
+    /// </summary>
+    public class V3ToV4VersionMigrationService(
+        ILogger<V3ToV4VersionMigrationService> logger,
+        CircleNetworkService circleNetworkService,
+        CircleDefinitionService circleDefinitionService)
+    {
+        public async Task UpgradeAsync(IOdinContext odinContext, CancellationToken cancellationToken)
+        {
+            odinContext.Caller.AssertHasMasterKey();
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            //
+            // Update the system circles to see new drive grants
+            //
+            logger.LogDebug("rebuilding system circles");
+            await circleDefinitionService.EnsureSystemCirclesExistAsync();
+
+            //
+            // This will reapply the grants
+            //
+            logger.LogDebug("Reapplying permissions for ConfirmedConnections Circle");
+            await circleNetworkService.UpdateCircleDefinitionAsync(SystemCircleConstants.ConfirmedConnectionsDefinition, odinContext);
+        }
+
+        public async Task ValidateUpgradeAsync(IOdinContext odinContext, CancellationToken cancellationToken)
+        {
+            var confirmedCircle = await circleDefinitionService.GetCircleAsync(SystemCircleConstants.ConfirmedConnectionsDefinition.Id);
+            foreach (var dg in SystemCircleConstants.ConfirmedConnectionsDefinition.DriveGrants)
+            {
+                if (null == confirmedCircle.DriveGrants.FirstOrDefault(cdg => cdg.PermissionedDrive == dg.PermissionedDrive))
+                {
+                    logger.LogError("Failed {cn} is missing drive grant {dg}", confirmedCircle.Name, dg.PermissionedDrive);
+                    throw new OdinSystemException($"Validation failed.  Confirmed circle missing drive grant {dg.PermissionedDrive}");
+                }
+                
+                logger.LogDebug("Validated {cn} has drive grant {dg}", confirmedCircle.Name, dg.PermissionedDrive);
+            }
+
+            var autoCircle = await circleDefinitionService.GetCircleAsync(SystemCircleConstants.AutoConnectionsSystemCircleDefinition.Id);
+            foreach (var dg in SystemCircleConstants.AutoConnectionsSystemCircleDefinition.DriveGrants)
+            {
+                if (null == autoCircle.DriveGrants.FirstOrDefault(cdg => cdg.PermissionedDrive == dg.PermissionedDrive))
+                {
+                    logger.LogError("Failed {cn} is missing drive grant {dg}", autoCircle.Name, dg.PermissionedDrive);
+                    throw new OdinSystemException($"Validation failed.  Auto-Connect circle missing drive grant {dg.PermissionedDrive}");
+                }
+                logger.LogDebug("Validated {cn} has drive grant {dg}", autoCircle.Name, dg.PermissionedDrive);
+
+            }
+        }
+    }
+}
