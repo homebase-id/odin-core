674968f2d0d73da4e56fc2454b8c85453cab55e7
toddmitchell <t@lighttheory.co>
2025-04-09 18:08:44 -0700

    replayed drive definition updated event to ensure anonymous drives are applied

--- src/services/Odin.Services/Configuration/VersionUpgrade/Version3tov4/V3ToV4VersionMigrationService.cs
+++ src/services/Odin.Services/Configuration/VersionUpgrade/Version3tov4/V3ToV4VersionMigrationService.cs
@@ -1,9 +1,13 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Odin.Core;
 using Odin.Core.Exceptions;
 using Odin.Services.Base;
+using Odin.Services.Drives.Management;
+using Odin.Services.Mediator;
 using Odin.Services.Membership.Circles;
 using Odin.Services.Membership.Connections;
 
@@ -15,25 +19,25 @@ namespace Odin.Services.Configuration.VersionUpgrade.Version3tov4
     public class V3ToV4VersionMigrationService(
         ILogger<V3ToV4VersionMigrationService> logger,
         CircleNetworkService circleNetworkService,
-        CircleDefinitionService circleDefinitionService)
+        CircleDefinitionService circleDefinitionService,
+        DriveManager driveManager)
     {
         public async Task UpgradeAsync(IOdinContext odinContext, CancellationToken cancellationToken)
         {
             odinContext.Caller.AssertHasMasterKey();
-
             cancellationToken.ThrowIfCancellationRequested();
 
-            //
-            // Update the system circles to see new drive grants
-            //
-            logger.LogDebug("rebuilding system circles");
-            await circleDefinitionService.EnsureSystemCirclesExistAsync();
+            var anonymousDrives = await driveManager.GetAnonymousDrivesAsync(PageOptions.All, odinContext);
 
-            //
-            // This will reapply the grants
-            //
-            logger.LogDebug("Reapplying permissions for ConfirmedConnections Circle");
-            await circleNetworkService.UpdateCircleDefinitionAsync(SystemCircleConstants.ConfirmedConnectionsDefinition, odinContext);
+            foreach (var drive in anonymousDrives.Results)
+            {
+                await circleNetworkService.Handle(new DriveDefinitionAddedNotification
+                {
+                    OdinContext = odinContext,
+                    IsNewDrive = false,
+                    Drive = drive
+                }, cancellationToken);
+            }
         }
 
         public async Task ValidateUpgradeAsync(IOdinContext odinContext, CancellationToken cancellationToken)
@@ -46,7 +50,7 @@ namespace Odin.Services.Configuration.VersionUpgrade.Version3tov4
                     logger.LogError("Failed {cn} is missing drive grant {dg}", confirmedCircle.Name, dg.PermissionedDrive);
                     throw new OdinSystemException($"Validation failed.  Confirmed circle missing drive grant {dg.PermissionedDrive}");
                 }
-                
+
                 logger.LogDebug("Validated {cn} has drive grant {dg}", confirmedCircle.Name, dg.PermissionedDrive);
             }
 
@@ -58,8 +62,8 @@ namespace Odin.Services.Configuration.VersionUpgrade.Version3tov4
                     logger.LogError("Failed {cn} is missing drive grant {dg}", autoCircle.Name, dg.PermissionedDrive);
                     throw new OdinSystemException($"Validation failed.  Auto-Connect circle missing drive grant {dg.PermissionedDrive}");
                 }
-                logger.LogDebug("Validated {cn} has drive grant {dg}", autoCircle.Name, dg.PermissionedDrive);
 
+                logger.LogDebug("Validated {cn} has drive grant {dg}", autoCircle.Name, dg.PermissionedDrive);
             }
         }
     }
