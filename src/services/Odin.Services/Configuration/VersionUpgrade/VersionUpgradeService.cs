97755c8f4d0b79003b9c87cd9d078362562d7715
toddmitchell <t@lighttheory.co>
2025-06-25 09:13:39 -0700

    testing feed reset

--- src/services/Odin.Services/Configuration/VersionUpgrade/VersionUpgradeService.cs
+++ src/services/Odin.Services/Configuration/VersionUpgrade/VersionUpgradeService.cs
@@ -10,6 +10,7 @@ using Odin.Services.Configuration.VersionUpgrade.Version0tov1;
 using Odin.Services.Configuration.VersionUpgrade.Version1tov2;
 using Odin.Services.Configuration.VersionUpgrade.Version2tov3;
 using Odin.Services.Configuration.VersionUpgrade.Version3tov4;
+using Odin.Services.Configuration.VersionUpgrade.Version4tov5;
 
 namespace Odin.Services.Configuration.VersionUpgrade;
 
@@ -20,7 +21,7 @@ public class VersionUpgradeService(
     V1ToV2VersionMigrationService v2,
     V2ToV3VersionMigrationService v3,
     V3ToV4VersionMigrationService v4,
-    // V4ToV5VersionMigrationService v5,
+    V4ToV5VersionMigrationService v5,
     OwnerAuthenticationService authService,
     ILogger<VersionUpgradeService> logger)
 {
@@ -131,16 +132,16 @@ public class VersionUpgradeService(
             
             if (currentVersion == 4)
             {
-                // _isRunning = true;
-                // logger.LogInformation("Upgrading from v{currentVersion}", currentVersion);
+                _isRunning = true;
+                logger.LogInformation("Upgrading from v{currentVersion}", currentVersion);
                 
-                // await v5.UpgradeAsync(odinContext, cancellationToken);
+                await v5.UpgradeAsync(odinContext, cancellationToken);
             
-                // await v5.ValidateUpgradeAsync(odinContext, cancellationToken);
+                await v5.ValidateUpgradeAsync(odinContext, cancellationToken);
             
-                // currentVersion = (await tenantConfigService.IncrementVersionAsync()).DataVersionNumber;
+                currentVersion = (await tenantConfigService.IncrementVersionAsync()).DataVersionNumber;
             
-                // logger.LogInformation("Upgrading to v{currentVersion} successful", currentVersion);
+                logger.LogInformation("Upgrading to v{currentVersion} successful", currentVersion);
             }
             
             // do this after each version upgrade
