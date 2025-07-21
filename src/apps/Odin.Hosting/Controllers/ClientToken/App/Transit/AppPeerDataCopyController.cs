7745bcb2c7f3df3acbcfa751a5d3d98b16d3a84a
toddmitchell <t@lighttheory.co>
2025-07-21 11:50:19 -0500

    peer data copy service wip

---
+++ src/apps/Odin.Hosting/Controllers/ClientToken/App/Transit/AppPeerDataCopyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Odin.Hosting.Controllers.Base;
+using Odin.Services.Peer.DataCopy;
+
+namespace Odin.Hosting.Controllers.ClientToken.App.Transit
+{
+    /// <summary>
+    /// Routes requests from the owner app to a target identity
+    /// </summary>
+    [ApiController]
+    [Route(AppApiPathConstants.PeerDataCopyV1)]
+    [AuthorizeValidAppToken]
+    public class AppPeerDataCopyController(PeerDataCopyService dataCopyService) : OdinControllerBase
+    {
+        [HttpPost("copy")]
+        public async Task<IActionResult> CopyDataFromPeer(CopyPeerDataRequest request)
+        {
+            var fst = GetHttpFileSystemResolver().GetFileSystemType();
+            var localFile = await dataCopyService.CopyFile(
+                request.RemoteIdentity,
+                request.SourceFileIdentifier, 
+                request.LocalDrive,
+                fst,
+                request.Overwrite,
+                WebOdinContext);
+
+            return Ok(localFile);
+        }
+    }
+}
