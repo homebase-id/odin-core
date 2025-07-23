9ee8b90543316f833bef67d61d0713cca88b8641
Sebastian Bargmann <sebbarg@gmail.com>
2025-07-23 14:23:54 +0200

    merge from main

--- src/core/Odin.Core.Storage/Database/System/Table/TableRegistrationsCRUD.cs
+++ src/core/Odin.Core.Storage/Database/System/Table/TableRegistrationsCRUD.cs
@@ -60,23 +60,23 @@ namespace Odin.Core.Storage.Database.System.Table
         }
 
 
-        public virtual async Task<int> EnsureTableExistsAsync(bool dropExisting = false)
+        public virtual async Task EnsureTableExistsAsync(bool dropExisting = false)
         {
             await using var cn = await _scopedConnectionFactory.CreateScopedConnectionAsync();
-            await using var cmd = cn.CreateCommand();
             if (dropExisting)
-            {
-                cmd.CommandText = "DROP TABLE IF EXISTS Registrations;";
-                await cmd.ExecuteNonQueryAsync();
-            }
+                await MigrationBase.DeleteTableAsync(cn, "Registrations");
             var rowid = "";
+            var commentSql = "";
             if (cn.DatabaseType == DatabaseType.Postgres)
+            {
                rowid = "rowid BIGSERIAL PRIMARY KEY,";
+               commentSql = "COMMENT ON TABLE Registrations IS '{ \"Version\": 0 }';";
+            }
             else
                rowid = "rowId INTEGER PRIMARY KEY AUTOINCREMENT,";
             var wori = "";
-            cmd.CommandText =
-                "CREATE TABLE IF NOT EXISTS Registrations("
+            string createSql =
+                "CREATE TABLE IF NOT EXISTS Registrations( -- { \"Version\": 0 }\n"
                    +rowid
                    +"identityId BYTEA NOT NULL UNIQUE, "
                    +"email TEXT , "
@@ -90,7 +90,7 @@ namespace Odin.Core.Storage.Database.System.Table
                    +"modified BIGINT NOT NULL "
                    +$"){wori};"
                    ;
-            return await cmd.ExecuteNonQueryAsync();
+            await MigrationBase.CreateTableAsync(cn, createSql, commentSql);
         }
 
         public virtual async Task<int> InsertAsync(RegistrationsRecord item)
