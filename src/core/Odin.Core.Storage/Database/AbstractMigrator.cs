2a1bdf5023c4a111e87aa64d50380724072ec752
Sebastian Bargmann <sebbarg@gmail.com>
2025-07-29 16:28:30 +0200

    Add abstract SortedMigrations

--- src/core/Odin.Core.Storage/Database/AbstractMigrator.cs
+++ src/core/Odin.Core.Storage/Database/AbstractMigrator.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
+
 namespace Odin.Core.Storage.Database;
 
 public abstract class AbstractMigrator
 {
-
+    protected abstract List<MigrationBase> SortedMigrations { get; }
 }
