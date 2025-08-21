7946b79283603c3b2739e1b96d110118409b542d
toddmitchell <t@lighttheory.co>
2025-08-21 09:10:08 -0500

    removed duplicate upgrade code

--- src/services/Odin.Services/Configuration/VersionUpgrade/Version5tov6/V5ToV6VersionMigrationService.cs
+++ src/services/Odin.Services/Configuration/VersionUpgrade/Version5tov6/V5ToV6VersionMigrationService.cs
@@ -1,7 +1,15 @@
-﻿using System.Threading;
+﻿using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Odin.Core.Exceptions;
+using Odin.Core.Storage.Database.Identity;
+using Odin.Services.Authorization.Apps;
 using Odin.Services.Base;
+using Odin.Services.Drives;
+using Odin.Services.Drives.Management;
+using Odin.Services.Membership.Circles;
+using Odin.Services.Membership.Connections;
 
 namespace Odin.Services.Configuration.VersionUpgrade.Version5tov6
 {
@@ -10,42 +18,85 @@ namespace Odin.Services.Configuration.VersionUpgrade.Version5tov6
     /// </summary>
     public class V5ToV6VersionMigrationService(
         ILogger<V5ToV6VersionMigrationService> logger,
-        TenantConfigService tenantConfigService)
+        TenantConfigService tenantConfigService,
+        CircleNetworkService circleNetworkService,
+        AppRegistrationService appRegistrationService,
+        IdentityDatabase db,
+        IDriveManager driveManager)
     {
         public async Task UpgradeAsync(IOdinContext odinContext, CancellationToken cancellationToken)
         {
-            logger.LogDebug("Preparing Password recovery Release for Identity [{identity}]", odinContext.Tenant);
-            await PreparePasswordRecoveryReleaseAsync(odinContext, cancellationToken);
+            logger.LogDebug("Preparing Shamira release 1 on identity: [{identity}]", odinContext.Tenant);
+            await tenantConfigService.EnsureSystemDrivesExist(odinContext);
+
+            await EnsureShardRecoveryDriveIsConfiguredForConnectedIdentitiesCircle(odinContext, cancellationToken);
         }
 
         public async Task ValidateUpgradeAsync(IOdinContext odinContext, CancellationToken cancellationToken)
         {
-            await ValidateIntroductionsReleaseAsync(odinContext, cancellationToken);
+            var shardDrive = await driveManager.GetDriveAsync(SystemDriveConstants.ShardRecoveryDrive.Alias);
+
+            if (shardDrive == null)
+            {
+                throw new OdinSystemException("Shard recovery drive not created");
+            }
+
+            // Get all ICRs and ensure they have write access to my Shard Recovery Drive
+            var allIdentities = await circleNetworkService.GetConnectedIdentitiesAsync(int.MaxValue, null, odinContext);
+
+            foreach (var identity in allIdentities.Results)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                if (!identity.AccessGrant.CircleGrants.TryGetValue(SystemCircleConstants.ConfirmedConnectionsCircleId, out var circleGrant))
+                {
+                    throw new OdinSystemException("Identity not granted access to ConfirmedConnectionsCircleId");
+                }
+
+                var driveGrant = circleGrant.KeyStoreKeyEncryptedDriveGrants
+                    .SingleOrDefault(g => g.PermissionedDrive.Drive == SystemDriveConstants.ShardRecoveryDrive);
+
+                if (driveGrant == null)
+                {
+                    throw new OdinSystemException("Drive grant for ShardRecoveryDrive not found");
+                }
+
+                var hasWrite = driveGrant.PermissionedDrive.Permission.HasFlag(DrivePermission.Write);
+                if (!hasWrite)
+                {
+                    throw new OdinSystemException("ShardRecoveryDrive not granted write permission");
+                }
+            }
         }
 
-        /// <summary>
-        /// Handles the changes to production data required for the introductions feature
-        /// </summary>
-        private async Task PreparePasswordRecoveryReleaseAsync(IOdinContext odinContext, CancellationToken cancellationToken)
+        private async Task EnsureShardRecoveryDriveIsConfiguredForConnectedIdentitiesCircle(IOdinContext odinContext,
+            CancellationToken cancellationToken)
         {
             odinContext.Caller.AssertHasMasterKey();
+            var allIdentities = await circleNetworkService.GetConnectedIdentitiesAsync(int.MaxValue, null, odinContext);
 
-            //
-            // Ensure all system drives exist (for older identities)
-            //
-            logger.LogDebug("Ensuring all system drives exist");
-            await tenantConfigService.EnsureSystemDrivesExist(odinContext);
-            cancellationToken.ThrowIfCancellationRequested();
-        }
-        
-        private async Task ValidateIntroductionsReleaseAsync(IOdinContext odinContext, CancellationToken cancellationToken)
-        {
-            odinContext.Caller.AssertHasMasterKey();
+            await using var tx = await db.BeginStackedTransactionAsync();
 
-            await Task.CompletedTask;
-            
-            //TODO verify system drive was created
-            cancellationToken.ThrowIfCancellationRequested();
+            foreach (var identity in allIdentities.Results)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                logger.LogDebug("Reconciling confirmed connections circle on Identity {odinId}", identity.OdinId);
+
+                var circleId = SystemCircleConstants.ConfirmedConnectionsCircleId;
+                await circleNetworkService.RevokeCircleAccessAsync(circleId, identity.OdinId, odinContext);
+                await circleNetworkService.GrantCircleAsync(circleId, identity.OdinId, odinContext);
+            }
+
+            var allApps = await appRegistrationService.GetRegisteredAppsAsync(odinContext);
+            foreach (var app in allApps)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                logger.LogDebug("Calling ReconcileAuthorizedCircles for app {appName}", app.Name);
+                await circleNetworkService.ReconcileAuthorizedCircles(oldAppRegistration: null, app, odinContext);
+            }
+
+            tx.Commit();
         }
     }
 }
