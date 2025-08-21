46660140f37598e6b68f81d78eb5a7b6451e143b
Sebastian Bargmann <sebbarg@gmail.com>
2025-08-21 14:08:19 +0200

    placeholders

---
+++ tests/core/Odin.Core.Storage.Tests/Database/Identity/Cache/TableCircleCachedTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using Odin.Core.Storage.Factory;
+
+namespace Odin.Core.Storage.Tests.Database.Identity.Cache;
+
+public class TableCircleCachedTests : IocTestBase
+{
+    [Test]
+    public async Task ItShouldBeImplemented()
+    {
+        await RegisterServicesAsync(DatabaseType.Sqlite);
+        await using var scope = Services.BeginLifetimeScope();
+        Assert.Fail("This test is not implemented yet.");
+    }
+
+    //
+
+}
+
+
