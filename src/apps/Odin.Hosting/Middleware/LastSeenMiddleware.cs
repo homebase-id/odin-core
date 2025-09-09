51de446e3c171dbe127597f580a472c7e9eab642
Sebastian Bargmann <sebbarg@gmail.com>
2025-09-09 12:37:06 +0200

    Update middelware

--- src/apps/Odin.Hosting/Middleware/LastSeenMiddleware.cs
+++ src/apps/Odin.Hosting/Middleware/LastSeenMiddleware.cs
@@ -1,11 +1,11 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Odin.Hosting.Authentication.YouAuth;
-using Odin.Services.Authentication.Owner;
 using Odin.Services.Base;
 using Odin.Services.LastSeen;
 
 namespace Odin.Hosting.Middleware;
+
+#nullable enable
 
 public class LastSeenMiddleware(RequestDelegate next)
 {
@@ -14,9 +14,7 @@ public class LastSeenMiddleware(RequestDelegate next)
         OdinContext odinContext,
         ILastSeenService lastSeen)
     {
-        if (odinContext.Caller.IsOwner &&
-            odinContext.Caller.OdinId.HasValue &&
-            odinContext.AuthContext is YouAuthConstants.AppSchemeName or OwnerAuthConstants.SchemeName)
+        if (odinContext.Caller.OdinId.HasValue)
         {
             await lastSeen.LastSeenNowAsync(odinContext.Caller.OdinId);
         }
