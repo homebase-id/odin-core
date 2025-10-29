6bef8d904e9e7bd7654687deb290126bfbe39cb3
Sebastian Bargmann <sebbarg@gmail.com>
2025-10-29 17:32:37 +0100

    Add JsonEnvelope

---
+++ src/core/Odin.Core/Json/JsonEnvelope.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Text.Json.Serialization;
+using Odin.Core.Exceptions;
+using Odin.Core.Serialization;
+
+namespace Odin.Core.Json;
+
+public class JsonEnvelope
+{
+    public required string MessageTypeName { get; init; }
+    public required string MessageJson { get; init; }
+
+    //
+
+    [JsonIgnore]
+    private Type? _cachedMessageType;
+    public Type GetMessageType()
+    {
+        return _cachedMessageType ??=
+            Type.GetType(MessageTypeName) ??
+            throw new OdinSystemException(
+                $"Failed to resolve type '{MessageTypeName}'. " +
+                "Ensure the type exists and its assembly is loaded.");
+    }
+
+    //
+
+    public object DeserializeMessage()
+    {
+        return OdinSystemSerializer.DeserializeOrThrow(MessageJson, GetMessageType());
+    }
+
+    //
+
+    public T DeserializeMessage<T>()
+    {
+        var instance = DeserializeMessage();
+        if (instance is not T typedInstance)
+        {
+            throw new OdinSystemException(
+                $"Expected type '{typeof(T).FullName}' but envelope contains '{MessageTypeName}'.");
+        }
+        return typedInstance;
+    }
+
+    //
+
+    public static JsonEnvelope Create(object message)
+    {
+        var messageType = message.GetType().AssemblyQualifiedName;
+        if (messageType == null)
+        {
+            throw new OdinSystemException(
+                $"Failed to get AssemblyQualifiedName for type '{message.GetType().FullName}'.");
+        }
+
+        var messageJson = OdinSystemSerializer.Serialize(message);
+        return new JsonEnvelope
+        {
+            MessageTypeName = messageType,
+            MessageJson = messageJson
+        };
+    }
+}
