ffde6caf2ed58af3261708ee2f6432dd2801a16e
Sebastian Bargmann <sebbarg@gmail.com>
2025-11-04 15:40:45 +0100

    Adding/improving db tx/cn diagnostics

--- src/core/Odin.Core.Storage/Database/KeyChain/KeyChainDatabase.cs
+++ src/core/Odin.Core.Storage/Database/KeyChain/KeyChainDatabase.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Immutable;
+﻿using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 using Autofac;
 using Odin.Core.Storage.Database.KeyChain.Connection;
-using Odin.Core.Storage.Database.KeyChain.Table;
+using Odin.Core.Storage.Database.System.Connection;
 using Odin.Core.Storage.Factory;
 
 namespace Odin.Core.Storage.Database.KeyChain;
 
+#nullable enable
+
+[SuppressMessage("ReSharper", "ExplicitCallerInfoArgument")]
 public partial class KeyChainDatabase(ILifetimeScope lifetimeScope) : AbstractDatabase<IKeyChainDbConnectionFactory>(lifetimeScope)
 {
     private readonly ILifetimeScope _lifetimeScope = lifetimeScope;
@@ -19,20 +24,26 @@ public partial class KeyChainDatabase(ILifetimeScope lifetimeScope) : AbstractDa
     //
     // Connection
     //
-    public override async Task<IConnectionWrapper> CreateScopedConnectionAsync()
+    public override async Task<IConnectionWrapper> CreateScopedConnectionAsync(
+        [CallerFilePath] string? filePath = null,
+        [CallerLineNumber] int lineNumber = 0)
     {
-        var factory = _lifetimeScope.Resolve<ScopedKeyChainConnectionFactory>();
-        var cn = await factory.CreateScopedConnectionAsync();
+        var factory = _lifetimeScope.Resolve<ScopedSystemConnectionFactory>();
+        var cn = await factory.CreateScopedConnectionAsync(filePath, lineNumber);
         return cn;
     }
 
     //
     // Transaction
     //
-    public override async Task<IScopedTransaction> BeginStackedTransactionAsync()
+    public override async Task<IScopedTransaction> BeginStackedTransactionAsync(
+        IsolationLevel isolationLevel = IsolationLevel.Unspecified,
+        CancellationToken cancellationToken = default,
+        [CallerFilePath] string? filePath = null,
+        [CallerLineNumber] int lineNumber = 0)
     {
-        var factory = _lifetimeScope.Resolve<ScopedKeyChainTransactionFactory>();
-        var tx = await factory.BeginStackedTransactionAsync();
+        var factory = _lifetimeScope.Resolve<ScopedSystemTransactionFactory>();
+        var tx = await factory.BeginStackedTransactionAsync(isolationLevel, cancellationToken, filePath, lineNumber);
         return tx;
     }
 
