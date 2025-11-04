71c12c18c2b8500d73959697aeceed1cc8de787c
Sebastian Bargmann <sebbarg@gmail.com>
2025-11-04 10:51:39 +0100

    Make test CanArchiveDriveAndDriveListIsCorrectlyReturned as Explicit

--- tests/apps/Odin.Hosting.Tests/OwnerApi/Drive/Management/DriveManagementArchiveTests.cs
+++ tests/apps/Odin.Hosting.Tests/OwnerApi/Drive/Management/DriveManagementArchiveTests.cs
@@ -57,7 +57,7 @@ public class DriveManagementArchiveTests
         yield return new object[] { new GuestWriteOnlyAccessToDrive(TargetDrive.NewTargetDrive()), false };
     }
 
-    [Test]
+    [Test, Explicit]
     [TestCaseSource(nameof(OwnerAllowed))]
     [TestCaseSource(nameof(AppAllowed))]
     [TestCaseSource(nameof(GuestAllowed))]
