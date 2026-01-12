a40206db6a715dabca945efdd99ff0697fd82a9c
Sebastian Bargmann <sebbarg@gmail.com>
2026-01-12 12:06:01 +0100

    Cdn test automation (#1380)

---
+++ src/apps/Odin.Hosting/UnifiedV2/Cdn/V2CdnController.cs
@@ -0,0 +1,31 @@
+﻿#nullable enable
+using Microsoft.AspNetCore.Mvc;
+using Odin.Hosting.Controllers.Base;
+using Odin.Hosting.UnifiedV2.Authentication.Policy;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Odin.Hosting.UnifiedV2.Cdn;
+
+// SEB:NOTE this controller is currently only used for CDN testing
+
+[ApiController]
+[Route(UnifiedApiRouteConstants.DrivesRoot)]
+[UnifiedV2Authorize(UnifiedPolicies.Anonymous)]
+[ApiExplorerSettings(GroupName = "v2")]
+public class V2CdnController : OdinControllerBase
+{
+    [HttpGet("cdn-ping/payload/cdn-ping")]
+    [SwaggerOperation(Tags = [SwaggerInfo.Auth])]
+    public ActionResult CdnPing()
+    {
+        return Ok();
+    }
+
+    [HttpGet("cdn-ping/bad-cdn-path/cdn-ping")]
+    [SwaggerOperation(Tags = [SwaggerInfo.Auth])]
+    public ActionResult CdnPingBadPath()
+    {
+        // SEB:NOTE this will never happen since CdnAuthPathHandler will reject the path before it reaches here
+        return Ok();
+    }
+}
---
+++ tests/apps/Odin.Hosting.Tests/_V2/ApiClient/CdnV2Client.cs
@@ -0,0 +1,27 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Odin.Core.Identity;
+using Odin.Hosting.Tests._Universal.ApiClient.Factory;
+using Refit;
+
+namespace Odin.Hosting.Tests._V2.ApiClient;
+
+public class CdnV2Client(OdinId identity, IApiClientFactory factory)
+{
+    public async Task<ApiResponse<HttpContent>> CdnPing()
+    {
+        var client = factory.CreateHttpClient(identity, out var sharedSecret);
+        var svc = RefitCreator.RestServiceFor<ICdnHttpClientApiV2>(client, sharedSecret);
+        var apiResponse = await svc.CdnPing();
+        return apiResponse;
+    }
+
+    public async Task<ApiResponse<HttpContent>> CdnPingBadPath()
+    {
+        var client = factory.CreateHttpClient(identity, out var sharedSecret);
+        var svc = RefitCreator.RestServiceFor<ICdnHttpClientApiV2>(client, sharedSecret);
+        var apiResponse = await svc.CdnPingBadPath();
+        return apiResponse;
+    }
+
+}
