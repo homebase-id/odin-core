445efe09d8be00209290301c6c56186477bb3779
Sebastian Bargmann <sebbarg@gmail.com>
2026-04-01 16:46:55 +0200

    Adding missing PageByRowId to system tables (#1449)

--- tests/core/Odin.Core.Storage.Tests/Database/System/Table/TableJobsTests.cs
+++ tests/core/Odin.Core.Storage.Tests/Database/System/Table/TableJobsTests.cs
@@ -191,6 +191,41 @@ public class TableJobsTests : IocTestBase
 
     //
 
+    //
+
+    [Test]
+    [TestCase(DatabaseType.Sqlite)]
+#if RUN_POSTGRES_TESTS
+    [TestCase(DatabaseType.Postgres)]
+#endif
+    public async Task ItShouldPageByRowId(DatabaseType databaseType)
+    {
+        await RegisterServicesAsync(databaseType);
+        await using var scope = Services.BeginLifetimeScope();
+        var jobs = scope.Resolve<TableJobs>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            var record = NewJobsRecord();
+            record.name = $"job{i}";
+            await jobs.InsertAsync(record);
+        }
+
+        var (page1, cursor1) = await jobs.PagingByRowIdAsync(2, null);
+        Assert.That(page1.Count, Is.EqualTo(2));
+        Assert.That(cursor1, Is.Not.Null);
+
+        var (page2, cursor2) = await jobs.PagingByRowIdAsync(2, cursor1);
+        Assert.That(page2.Count, Is.EqualTo(1));
+        Assert.That(cursor2, Is.Null);
+
+        var (all, allCursor) = await jobs.PagingByRowIdAsync(100, null);
+        Assert.That(all.Count, Is.EqualTo(3));
+        Assert.That(allCursor, Is.Null);
+    }
+
+    //
+
     private JobsRecord NewJobsRecord()
     {
         return new JobsRecord
