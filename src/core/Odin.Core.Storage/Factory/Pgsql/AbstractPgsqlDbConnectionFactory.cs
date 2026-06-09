86a72bbe0afce9af1ff9fd4a755abb04f7eb01db
Sebastian Bargmann <sebbarg@gmail.com>
2026-06-09 11:50:42 +0200

    PG: KeepAlive = 30 (#1555)

--- src/core/Odin.Core.Storage/Factory/Pgsql/AbstractPgsqlDbConnectionFactory.cs
+++ src/core/Odin.Core.Storage/Factory/Pgsql/AbstractPgsqlDbConnectionFactory.cs
@@ -1,17 +1,25 @@
 using System.Data.Common;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace Odin.Core.Storage.Factory.Pgsql;
 
 public abstract class AbstractPgsqlDbConnectionFactory(string connectionString)
 {
+    private readonly string _connectionString = new NpgsqlConnectionStringBuilder(connectionString)
+    {
+        KeepAlive = 30,
+    }.ConnectionString;
+
+    //
+
     public DatabaseType DatabaseType => DatabaseType.Postgres;
 
     //
 
     public async Task<DbConnection> OpenAsync()
     {
-        return await PgsqlConcreteConnectionFactory.CreateAsync(connectionString);
+        return await PgsqlConcreteConnectionFactory.CreateAsync(_connectionString);
     }
 
     //
